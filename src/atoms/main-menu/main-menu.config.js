7480a590f415ae87d76eca34b03c5c80593e6533
rhedman <robert.hedman@creuna.se>
2018-11-27 10:06:20 +0100

    fixed card bug and added main menu atom

---
+++ src/atoms/main-menu/main-menu.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+	status: 'wip',
+
+	context: {
+		item: [
+			{
+				text: 'Kunskap',
+				url: '#',
+				is_current: false
+			},
+			{
+				text: 'Innovation',
+				url: '#',
+				is_current: false
+			},
+			{
+				text: 'Domäner',
+				url: '#',
+				is_current: true
+			},
+			{
+				text: 'Om oss',
+				url: '#',
+				is_current: false
+			}
+		]
+	}
+}
