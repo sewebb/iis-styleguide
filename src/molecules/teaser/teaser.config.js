c978d5ed198fcd74840f8d8ead79727e3a8920ce
rhedman <robert.hedman@creuna.se>
2018-12-04 09:01:07 +0100

    removed node from content json

--- src/molecules/teaser/teaser.config.js
+++ src/molecules/teaser/teaser.config.js
@@ -2,7 +2,6 @@ module.exports = {
 	status: 'wip',
 
 	context: {
-		text: 'Fler nyheter',
 		url: 'http://www.iis.se',
 		link_text: 'Nätneutraliteten och den svenska lagstiftningen',
 		background_image: '/assets/images/KPA_5090-5-4.jpg',
