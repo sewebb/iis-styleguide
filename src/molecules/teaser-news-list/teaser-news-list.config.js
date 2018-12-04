690cd132b49cd37909e077d3f4ae32945e0b4ce2
rhedman <robert.hedman@creuna.se>
2018-12-04 10:42:53 +0100

    added teaser news list

---
+++ src/molecules/teaser-news-list/teaser-news-list.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+	status: 'wip',
+
+	context: {
+		title: 'Nyheter',
+		link_text: 'Nätneutraliteten och den svenska lagstiftningen',
+		background_image: '/assets/images/KPA_5090-5-4.jpg',
+		items: [
+			{
+				url: 'http://www.iis.se',
+				heading: 'Internetdagarna 2019',
+				time: '2018-12-04',
+				tags: [
+					{
+						text: 'stockholm',
+						url: '#'
+					},
+					{
+						text: 'göteborg',
+						url: '#'
+					}
+				]
+			},
+			{
+				url: 'http://www.iis.se',
+				heading: 'Lansering av Svenskarna och internet 2019',
+				time: '2019-01-22, Tid: 10:00',
+				tags: false
+			},
+			{
+				url: 'http://www.iis.se',
+				heading: 'Så säkrar du dina konton',
+				time: '2019-05-13',
+				tags: [
+					{
+						text: 'stockholm',
+						url: '#'
+					},
+					{
+						text: 'göteborg',
+						url: '#'
+					}
+				]
+			}
+		]
+	}
+}
