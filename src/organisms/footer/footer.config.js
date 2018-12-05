9e1925cb7fd522d2d8fd5604b32009cc0062a3ce
rhedman <robert.hedman@creuna.se>
2018-12-05 09:50:47 +0100

    added POC for footer

---
+++ src/organisms/footer/footer.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	status: 'wip',
+
+	context: {
+		footer_text: '<p><a href="#">Internetstiftelsen</a> verkar för ett internet som bidrar positivt till människan och samhället. Vi är en oberoende organisation som säkerställer en stark och säker infrastruktur för internet i Sverige. Vårt mål är att alla ska våga och kunna använda internet. Vi finns i Sverige och ansvarar för den svenska toppdomänen .se och driften av toppdomänen .nu.</p>',
+		contact_1: '<ul class="u-list-clean"><li>Internetstiftelsen i Sverige</li><li>Hammarby Kaj 10D (<a href="#">karta</a>)</li><li>box 92073</li><li>120 07 Stockholm</li></ul>',
+		contact_2: '<ul class="u-list-clean"><li>E-post: <a href="mailto:info@internetstiftelsen.se</a></li><li>Telefon: <a href="tel:084523500">08 452 35 00</a></li><li>Fax: 08 452 35 00</li><li>Org.nr 802405-0190</li></ul>'
+	}
+}
