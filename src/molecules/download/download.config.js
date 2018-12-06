ca032df2ee591f118c6fc8adffba83dee8eb8f9e
rhedman <robert.hedman@creuna.se>
2018-12-06 10:03:55 +0100

    added download block

---
+++ src/molecules/download/download.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	status: 'wip',
+
+	context: {
+		url: 'https://www.iis.se/docs/Remiss_se_sv.pdf',
+		link_text: 'Svenskarna och internet 2019',
+		description: 'Rapport om Svenskarnas internetanvändande i pdf-format',
+		background_image: '/assets/images/KPA_5090-5-4.jpg',
+		download: 'Ladda ner',
+		icon: 'arrow-forwards'
+	}
+}
