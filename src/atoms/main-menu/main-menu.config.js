ce9bd7536ffa28f825f86e91ad9286deb4d0f18b
rhedman <robert.hedman@creuna.se>
2018-12-19 13:18:12 +0100

    design changes to main menu and header

--- src/atoms/main-menu/main-menu.config.js
+++ src/atoms/main-menu/main-menu.config.js
@@ -6,22 +6,57 @@ module.exports = {
 			{
 				text: 'Kunskap',
 				url: '#',
-				is_current: false
+				is_current: false,
+				icon: false,
+				hide_mobile: true
 			},
 			{
 				text: 'Innovation',
 				url: '#',
-				is_current: false
+				is_current: false,
+				icon: false,
+				hide_mobile: true
 			},
 			{
 				text: 'Domäner',
 				url: '#',
-				is_current: true
+				is_current: true,
+				icon: false,
+				hide_mobile: true
 			},
 			{
 				text: 'Om oss',
 				url: '#',
-				is_current: false
+				is_current: false,
+				icon: false,
+				hide_mobile: true
+			},
+			{
+				text: 'Sök ledig domän',
+				url: false,
+				is_current: false,
+				icon: 'arrow-down',
+				has_expandable: true,
+				controls: 'domain-search',
+				hide_mobile: true
+			},
+			{
+				text: 'Sök',
+				url: false,
+				is_current: false,
+				icon: 'search',
+				has_expandable: true,
+				controls: 'site-search',
+				hide_mobile: false
+			},
+			{
+				text: 'Meny',
+				url: false,
+				is_current: false,
+				icon: 'hamburger',
+				has_expandable: true,
+				controls: 'mega-menu',
+				hide_mobile: false
 			}
 		]
 	}
--- src/organisms/header/header.config.js
+++ src/organisms/header/header.config.js
@@ -3,38 +3,6 @@ module.exports = {
 
 	context: {
 		logo_link_text: 'Till startsidan',
-		logo_link_href: '/',
-		item: [
-			{
-				text: 'Kunskap',
-				url: '#',
-				is_current: false
-			},
-			{
-				text: 'Innovation',
-				url: '#',
-				is_current: false
-			},
-			{
-				text: 'Domäner',
-				url: '#',
-				is_current: true
-			},
-			{
-				text: 'Om oss',
-				url: '#',
-				is_current: false
-			},
-			{
-				text: 'Sök ledig domän',
-				url: false,
-				is_current: false
-			},
-			{
-				text: 'Sök',
-				url: false,
-				is_current: false
-			}
-		]
+		logo_link_href: '/'
 	}
 }
