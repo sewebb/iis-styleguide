be02785216d654922765ed385021ea6045d1de53
rhedman <robert.hedman@creuna.se>
2019-01-02 13:41:03 +0100

    added molecule form-control with modifiers for large and small form fields

---
+++ src/molecules/form-control/form-control.config.js
@@ -0,0 +1,77 @@
+module.exports = {
+	status: 'wip',
+
+	context: {
+		label: 'Namn',
+		labelClasses: 'is-required',
+		type: 'text',
+		id: 'name',
+		value: false,
+		placeholder: 'Förnamn',
+		autocomplete: 'off',
+		has_icon: true,
+		required: true,
+		disabled: false,
+		button_type: 'button',
+		button_text: 'Sök',
+		checkbox_value: '1',
+		checkbox_id: 'checkbox',
+		checkbox_name: 'checkbox',
+		radiobutton_id: 'radiobutton',
+		radiobutton_name: 'radiobutton',
+		options: [
+			{
+				value: '',
+				text:'Välj',
+				is_disabled: true,
+				is_selected: true
+			},
+			{
+				value: 'value-1',
+				text:'Option 1',
+				is_disabled: false,
+				is_selected: false
+			},
+			{
+				value: 'value-2',
+				text:'Option 2',
+				is_disabled: false,
+				is_selected: false
+			},
+			{
+				value: 'value-3',
+				text:'Option 3',
+				is_disabled: false,
+				is_selected: false
+			},
+			{
+				value: 'value-4',
+				text:'Option 4',
+				is_disabled: false,
+				is_selected: false
+			}
+		]
+	},
+	variants: [
+		{
+			name: 'Large',
+			context: {
+				modifier_class: 'm-form-control--large',
+				checkbox_id: 'checkbox-large',
+				checkbox_name: 'checkbox-large',
+				radiobutton_id: 'radiobutton-large',
+				radiobutton_name: 'radiobutton-large'
+			}
+		},
+		{
+			name: 'Small',
+			context: {
+				modifier_class: 'm-form-control--small',
+				checkbox_id: 'checkbox-small',
+				checkbox_name: 'checkbox-small',
+				radiobutton_id: 'radiobutton-small',
+				radiobutton_name: 'radiobutton-small'
+			}
+		}
+	]
+}
---
+++ src/molecules/form-control/readme.md
@@ -0,0 +1,7 @@
+# Form control
+
+Form control is used to manage form element sizes. There are two (2) modifier classes available to make form fields smaller or larger:
+* ```.m-form-control--large```
+* ```.m-form-control--small```
+
+When applied, all form fields within will adapt their sizes accordingly
