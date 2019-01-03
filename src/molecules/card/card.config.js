6815965813126c95b074d2f09dcf024698a396d8
rhedman <robert.hedman@creuna.se>
2019-01-03 08:55:56 +0100

    added support for formatted exerpt and button in card

--- src/molecules/card/card.config.js
+++ src/molecules/card/card.config.js
@@ -6,9 +6,10 @@ module.exports = {
 		img_url: '/assets/images/ALX_0876.jpg',
 		title: 'Gymnasieskolor med hög bandbredd har bättre skolresultat',
 		url: 'http://www.google.se',
-		exerpt: 'Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Faucibus a pellentesque sit amet porttitor.',
+		exerpt: 'Eget nulla <strong>facilisi etiam</strong> dignissim diam quis enim lobortis scelerisque. Faucibus a pellentesque sit amet porttitor.',
 		is_padded: true,
 		is_wide: true,
-		has_shadow: false
+		has_shadow: false,
+		has_button: false
 	}
 }
