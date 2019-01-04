bfea2197526c968c5651004ae0d7760e55f484cd
rhedman <robert.hedman@creuna.se>
2019-01-04 08:25:44 +0100

    included a11y scripts via npm and moved accordion config to separate file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 require('./utilities/tab-highlighting/tab-highlighting');
 require('./atoms/grid-toggle/grid-toggle');
-require('./vendor/toggle/a11y.toggle.min.js');
-require('./vendor/toggle/a11y.toggle-switcher.js');
-require('./vendor/accordion/van11y-accessible-accordion-aria.es6');
+require('../node_modules/a11y-toggle/a11y-toggle');
+require('./vendor/toggle/a11y.toggle-switcher');
+require('../node_modules/van11y-accessible-accordion-aria/dist/van11y-accessible-accordion-aria.es6');
+// require('./vendor/accordion/van11y-accessible-accordion-aria.es6');
+require('./organisms/accordion/accordion');
---
+++ src/organisms/accordion/accordion.js
@@ -0,0 +1,9 @@
+const oAccordion = window.van11yAccessibleAccordionAria({
+	ACCORDION_PREFIX_IDS: 'o-accordion',
+	ACCORDION_JS: 'js-o-accordion',
+	ACCORDION_STYLE: 'o-accordion',
+	ACCORDION_TITLE_STYLE: 'o-accordion__title',
+	ACCORDION_HEADER_STYLE: 'o-accordion__header',
+	ACCORDION_PANEL_STYLE: 'o-accordion__panel',
+});
+oAccordion.attach();
