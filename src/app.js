8776fcd3cadbddd83cd44b614b58cba1ea8cb50c
rhedman <robert.hedman@creuna.se>
2019-01-04 09:06:05 +0100

    changed the way a11y/van11y packages are included in the project

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 require('./utilities/tab-highlighting/tab-highlighting');
 require('./atoms/grid-toggle/grid-toggle');
-require('../node_modules/a11y-toggle/a11y-toggle');
+require('a11y-toggle');
 require('./vendor/toggle/a11y.toggle-switcher');
-require('../node_modules/van11y-accessible-accordion-aria/dist/van11y-accessible-accordion-aria.es6');
-// require('./vendor/accordion/van11y-accessible-accordion-aria.es6');
+require('van11y-accessible-accordion-aria');
 require('./organisms/accordion/accordion');
