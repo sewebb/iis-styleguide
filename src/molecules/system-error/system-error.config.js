87621fe505d4a511948738ca678dca7138753dab
rhedman <robert.hedman@creuna.se>
2019-01-04 14:02:53 +0100

    added system-error molecule

---
+++ src/molecules/system-error/system-error.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	status: 'wip',
+
+	context: {
+		errorCode: '404',
+		preamble: 'Sidan du eftersökte kunde tyvärr inte hittas.',
+		suggestion: 'Försök <a href="javascript:history.go(-1)">backa i webbläsaren</a> eller gå till <a href="/">startsidan</a>.',
+		errorMessage: 'error - Sidan kan inte hittas'
+	}
+}
