cf6e48a485bb55b513001423a7c78263a672c2a4
rhedman <robert.hedman@creuna.se>
2019-01-10 15:35:08 +0100

    started with filter component

---
+++ src/organisms/filter/filter.config.js
@@ -0,0 +1,165 @@
+module.exports = {
+	status: 'wip',
+
+	context: {
+		heading: 'Filtrera',
+		is_expanded: false,
+		checkboxes1: [
+			{
+				id: 'film',
+				label: 'Film',
+				value: '0',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'rapport',
+				label: 'Rapport',
+				value: '1',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'internetguider',
+				label: 'Internetguider',
+				value: '2',
+				is_inline: false,
+				is_disabled: false
+			}
+		],
+		checkboxes2: [
+			{
+				id: 'barnhack',
+				label: 'Barnhack',
+				value: '3',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'domämnnamn',
+				label: 'Domämnnamn',
+				value: '4',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'it-säkerhet',
+				label: 'IT-säkerhet',
+				value: '5',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'statistik',
+				label: 'Statistik',
+				value: '6',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'dns',
+				label: 'DNS',
+				value: '7',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'integritet',
+				label: 'Integritet',
+				value: '8',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'skola',
+				label: 'Skola',
+				value: '9',
+				is_inline: false,
+				is_disabled: false
+			}
+		],
+		checkboxes3: [
+			{
+				id: 'barn och unga',
+				label: 'Barn coh unga',
+				value: '10',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'företagare',
+				label: 'Företagare',
+				value: '11',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'pedagoger',
+				label: 'Pedagoger',
+				value: '12',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'föräldrar',
+				label: 'Föräldrar',
+				value: '13',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'journalister',
+				label: 'Journalister',
+				value: '14',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'privatpersoner',
+				label: 'Privatpersoner',
+				value: '15',
+				is_inline: false,
+				is_disabled: false
+			}
+		],
+		checkboxes4: [
+			{
+				id: 'iis',
+				label: 'IIS',
+				value: '16',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'pts',
+				label: 'PTS',
+				value: '17',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'scb',
+				label: 'SCB',
+				value: '18',
+				is_inline: false,
+				is_disabled: false
+			},
+			{
+				id: 'statens medieråd',
+				label: 'Statens medieråd',
+				value: '19',
+				is_inline: false,
+				is_disabled: false
+			}
+		],
+		checkboxes5: [
+			{
+				id: 'visa arkiverat material',
+				label: 'Visa arkiverat material',
+				value: '20',
+				is_inline: false,
+				is_disabled: false
+			}
+		]
+	}
+}
