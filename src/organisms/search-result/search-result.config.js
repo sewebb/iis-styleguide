cc21ae46fa162f731355331f8a5b31e132a37720
rhedman <robert.hedman@creuna.se>
2019-01-10 08:45:16 +0100

    further work on search result

--- src/organisms/search-result/search-result.config.js
+++ src/organisms/search-result/search-result.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	context: {
 		label: 'sök på internetstiftelsen.se',
 		alt: 'Hands typing on laptop',
-		img_url: '/assets/images/ALX_0876.jpg',
+		img_url: '/assets/images/search-hit.png',
 		title: 'Gymnasieskolor med hög bandbredd har bättre skolresultat',
 		url: 'http://www.google.se',
 		exerpt: 'Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Faucibus a pellentesque sit amet porttitor.'
