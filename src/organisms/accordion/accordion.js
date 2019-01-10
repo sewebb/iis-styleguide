04c11841e825dc1f42236bf2caa67fe1fe6ff4a3
rhedman <robert.hedman@creuna.se>
2019-01-10 13:50:51 +0100

    added support for namespace in accordion component

--- src/organisms/accordion/accordion.js
+++ src/organisms/accordion/accordion.js
@@ -1,9 +1,23 @@
+// const oAccordion = window.van11yAccessibleAccordionAria({
+// 	ACCORDION_PREFIX_IDS: 'o-accordion',
+// 	ACCORDION_JS: 'js-o-accordion',
+// 	ACCORDION_STYLE: 'o-accordion',
+// 	ACCORDION_TITLE_STYLE: 'o-accordion__title',
+// 	ACCORDION_HEADER_STYLE: 'o-accordion__header',
+// 	ACCORDION_PANEL_STYLE: 'o-accordion__panel',
+// });
+// oAccordion.attach();
+
+
+const accordionElement = document.querySelector('.js-o-accordion');
+let namespace = getComputedStyle(accordionElement, ':before').content;
+namespace = namespace.replace(/["']/g, '');
 const oAccordion = window.van11yAccessibleAccordionAria({
 	ACCORDION_PREFIX_IDS: 'o-accordion',
 	ACCORDION_JS: 'js-o-accordion',
-	ACCORDION_STYLE: 'o-accordion',
-	ACCORDION_TITLE_STYLE: 'o-accordion__title',
-	ACCORDION_HEADER_STYLE: 'o-accordion__header',
-	ACCORDION_PANEL_STYLE: 'o-accordion__panel',
+	ACCORDION_STYLE: `${namespace}o-accordion`,
+	ACCORDION_TITLE_STYLE: `${namespace}o-accordion__title`,
+	ACCORDION_HEADER_STYLE: `${namespace}o-accordion__header`,
+	ACCORDION_PANEL_STYLE: `${namespace}o-accordion__panel`,
 });
 oAccordion.attach();
