a060c1f269bf9b66bdad1f1a38264d87d0eb1e0c
rhedman <robert.hedman@creuna.se>
2019-01-15 15:20:26 +0100

    foxed layout on search result and added info-box component

--- src/organisms/search-result/search-result.config.js
+++ src/organisms/search-result/search-result.config.js
@@ -4,8 +4,8 @@ module.exports = {
 	context: {
 		label: 'sök på internetstiftelsen.se',
 		alt: 'Hands typing on laptop',
-		img_url: '/assets/images/search-hit.png',
-		title: 'Gymnasieskolor med hög bandbredd har bättre skolresultat',
+		img_url: '/assets/images/ALX_0876.jpg',
+		title: 'Man spelar Candy Crush',
 		url: 'http://www.google.se',
 		exerpt: 'Praesent tempus metus in arcu mollis, non convallis odio faucibus. Nullam lectus nulla, rutrum nec odio ut, mollis interdum enim. Proin tincidunt odio at quam eleifend imperdiet. Mauris sagittis nulla ipsum, consectetur efficitur libero dignissim in.'
 	}
