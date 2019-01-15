75a22e7157d0f8b2eadf76c59e1a607d58903efc
Tobias Bleckert <tobias.bleckert@iis.se>
2019-01-15 11:52:14 +0100

    Grid toggle is style guide specific

--- src/app.js
+++ src/app.js
@@ -1,8 +1,2 @@
-require('./utilities/tab-highlighting/tab-highlighting');
 require('./atoms/grid-toggle/grid-toggle');
-require('./molecules/system-error/system-error');
-require('a11y-toggle');
-require('./vendor/toggle/a11y.toggle-switcher');
-require('./organisms/accordion/accordion');
-require('./organisms/domain-search/domain-search');
-require('./organisms/mega-menu/mega-menu');
+require('./globals');
