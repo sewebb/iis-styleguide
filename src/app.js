552562437318edb49126d09909be3901caad808d
Tobias Bleckert <tobias.bleckert@iis.se>
2019-01-15 11:49:22 +0100

    Check if accordions exists before working with them

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ require('./atoms/grid-toggle/grid-toggle');
 require('./molecules/system-error/system-error');
 require('a11y-toggle');
 require('./vendor/toggle/a11y.toggle-switcher');
-require('van11y-accessible-accordion-aria');
 require('./organisms/accordion/accordion');
 require('./organisms/domain-search/domain-search');
 require('./organisms/mega-menu/mega-menu');
--- src/organisms/accordion/accordion.js
+++ src/organisms/accordion/accordion.js
@@ -1,24 +1,19 @@
-// const oAccordion = window.van11yAccessibleAccordionAria({
-// 	ACCORDION_PREFIX_IDS: 'o-accordion',
-// 	ACCORDION_JS: 'js-o-accordion',
-// 	ACCORDION_STYLE: 'o-accordion',
-// 	ACCORDION_TITLE_STYLE: 'o-accordion__title',
-// 	ACCORDION_HEADER_STYLE: 'o-accordion__header',
-// 	ACCORDION_PANEL_STYLE: 'o-accordion__panel',
-// });
-// oAccordion.attach();
+require('van11y-accessible-accordion-aria');
 
+const className = 'o-accordion';
+const accordionElement = document.querySelector(`.js-${className}`);
 
-const accordionElement = document.querySelector('.js-o-accordion');
-let namespace = getComputedStyle(accordionElement, ':before').content;
-console.log(namespace);
-namespace = namespace.replace(/["']/g, '');
-const oAccordion = window.van11yAccessibleAccordionAria({
-	ACCORDION_PREFIX_IDS: 'o-accordion',
-	ACCORDION_JS: 'js-o-accordion',
-	ACCORDION_STYLE: `${namespace}o-accordion`,
-	ACCORDION_TITLE_STYLE: `${namespace}o-accordion__title`,
-	ACCORDION_HEADER_STYLE: `${namespace}o-accordion__header`,
-	ACCORDION_PANEL_STYLE: `${namespace}o-accordion__panel`,
-});
-oAccordion.attach();
+if (accordionElement) {
+	const namespace = getComputedStyle(accordionElement, ':before').content.replace(/["']/g, '');
+
+	const oAccordion = window.van11yAccessibleAccordionAria({
+		ACCORDION_PREFIX_IDS: className,
+		ACCORDION_JS: `js-${className}`,
+		ACCORDION_STYLE: `${namespace}${className}`,
+		ACCORDION_TITLE_STYLE: `${namespace}${className}__title`,
+		ACCORDION_HEADER_STYLE: `${namespace}${className}__header`,
+		ACCORDION_PANEL_STYLE: `${namespace}${className}__panel`,
+	});
+
+	oAccordion.attach();
+}
