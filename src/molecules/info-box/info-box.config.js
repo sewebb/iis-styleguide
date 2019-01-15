f61dbe0b53e242d1713770d0d152e102d2121fb2
rhedman <robert.hedman@creuna.se>
2019-01-15 15:23:40 +0100

    added missing reasource to git

---
+++ src/molecules/info-box/info-box.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	status: 'wip',
+
+	context: {
+		text: 'Sed malesuada quis risus eget malesuada. <strong>Vivamus posuere</strong>, erat sit amet vehicula suscipit, nisi arcu euismod sem, id tempus justo ligula sit amet turpis. <a href="#">Sed faucibus purus</a> sed neque pulvinar, ac faucibus leo tristique.'
+	}
+}
