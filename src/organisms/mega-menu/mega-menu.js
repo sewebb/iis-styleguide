7452fd51652ed09536e133d0a5068721ebdd1f0d
rhedman <robert.hedman@creuna.se>
2019-01-17 11:10:42 +0100

    started implmenting footer animation

--- src/organisms/mega-menu/mega-menu.js
+++ src/organisms/mega-menu/mega-menu.js
@@ -1,7 +1,12 @@
 const containerToggle = require('../../ContainerToggle');
+const nav = require('../footer/footer');
 
 const megaMenuButton = document.querySelector('.js-toggle-mega-menu');
 
 if (megaMenuButton) {
 	containerToggle(megaMenuButton);
 }
+
+megaMenuButton.addEventListener('click', nav.animate);
+
+window.addEventListener('resize', nav.animate);
