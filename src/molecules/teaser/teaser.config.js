0cb950eabfbc62360e02e54cc6f50fadb9f55467
rhedman <robert.hedman@creuna.se>
2019-01-24 14:00:17 +0100

    small changes to Teaser

--- src/molecules/teaser/teaser.config.js
+++ src/molecules/teaser/teaser.config.js
@@ -5,6 +5,16 @@ module.exports = {
 		url: 'http://www.iis.se',
 		link_text: 'Nätneutraliteten och den svenska lagstiftningen',
 		background_image: '/assets/images/KPA_5090-5-4.jpg',
-		icon: 'arrow-forwards'
+		icon: 'arrow-forwards',
+
+		alt: 'Hands typing on laptop',
+		img_url: '/assets/images/ALX_0876.jpg',
+		title: 'Gymnasieskolor med hög bandbredd har bättre skolresultat',
+		exerpt: 'Eget nulla <strong>facilisi etiam</strong> dignissim diam quis enim lobortis scelerisque. Faucibus a pellentesque sit amet porttitor.',
+		is_padded: true,
+		is_wide: true,
+		has_shadow: false,
+		has_button: false,
+		read_time: true
 	}
 }
