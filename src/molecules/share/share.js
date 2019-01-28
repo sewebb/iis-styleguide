c7b193603ee74f1c49be712377e8c9c1c3c766db
rhedman <robert.hedman@creuna.se>
2019-01-28 10:48:53 +0100

    adjusted popup size for LinkedIn

--- src/molecules/share/share.js
+++ src/molecules/share/share.js
@@ -1,8 +1,8 @@
 function openPopup(e) {
 	e.preventDefault();
 
-	const width = 575;
-	const height = 400;
+	const width = 500;
+	const height = 470;
 	const left = window.innerWidth / 2 - width / 2;
 	const top = window.innerHeight / 2 - height / 2;
 	const url = this.href;
