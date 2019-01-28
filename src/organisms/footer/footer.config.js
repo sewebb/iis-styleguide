dceb35ee5568b3e3605895e3951dcaccb3b568e1
rhedman <robert.hedman@creuna.se>
2019-01-28 11:40:22 +0100

    added ISO-logo and link to footer

--- src/organisms/footer/footer.config.js
+++ src/organisms/footer/footer.config.js
@@ -5,6 +5,9 @@ module.exports = {
 		id: '',
 		footer_text: '<p><a href="#">Internetstiftelsen</a> verkar för ett internet som bidrar positivt till människan och samhället. Vi är en oberoende organisation som säkerställer en stark och säker infrastruktur för internet i Sverige. Vårt mål är att alla ska våga och kunna använda internet. Vi finns i Sverige och ansvarar för den svenska toppdomänen .se och driften av toppdomänen .nu.</p>',
 		contact_1: '<ul class="u-list-clean"><li>Internetstiftelsen i Sverige</li><li>Hammarby Kaj 10D (<a href="#">karta</a>)</li><li>box 92073</li><li>120 07 Stockholm</li></ul>',
-		contact_2: '<ul class="u-list-clean"><li>E-post: <a href="mailto:info@internetstiftelsen.se</a></li><li>Telefon: <a href="tel:084523500">08 452 35 00</a></li><li>Fax: 08 452 35 00</li><li>Org.nr 802405-0190</li></ul>'
+		contact_2: '<ul class="u-list-clean"><li>E-post: <a href="mailto:info@internetstiftelsen.se</a></li><li>Telefon: <a href="tel:084523500">08 452 35 00</a></li><li>Fax: 08 452 35 00</li><li>Org.nr 802405-0190</li></ul>',
+		ISO_link: 'https://www.iis.se/docs/Certifikat_27001_UKAS_sv.pdf',
+		ISO_image: '/assets/images/ISO_27001_2013_black_TM.svg',
+		ISO_text: 'Certifierade enligt ISO/IEC<br>27001:2013',
 	}
 }
