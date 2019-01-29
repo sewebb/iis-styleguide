d5ab101129b74aea9797baa1afd5e6d54ec688cb
rhedman <robert.hedman@creuna.se>
2019-01-29 10:50:20 +0100

    removed margin when p is last child

--- src/organisms/hero/hero.config.js
+++ src/organisms/hero/hero.config.js
@@ -4,6 +4,7 @@ module.exports = {
 	context: {
 		heading: 'Missa inte lanseringen av Svenskarna och internet',
 		text: 'Internetstiftelsen lanserar ett nytt innovativt verktyg för att avslöja trollen på internet. Och nu behöver vi behöver din hjälp att jaga trollen.',
-		additional_classes: 'o-hero--no-image'
+		additional_classes: 'o-hero--no-image',
+		has_image: false
 	}
 }
