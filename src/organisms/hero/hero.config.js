67509663ddf2588fdbde5a453efd7728f43e739f
rhedman <robert.hedman@creuna.se>
2019-01-29 08:20:11 +0100

    added font smoothing to white texts

--- src/organisms/hero/hero.config.js
+++ src/organisms/hero/hero.config.js
@@ -4,6 +4,6 @@ module.exports = {
 	context: {
 		heading: 'Missa inte lanseringen av Svenskarna och internet',
 		text: 'Internetstiftelsen lanserar ett nytt innovativt verktyg för att avslöja trollen på internet. Och nu behöver vi behöver din hjälp att jaga trollen.',
-		additional_classes: ''
+		additional_classes: 'o-hero--no-image'
 	}
 }
