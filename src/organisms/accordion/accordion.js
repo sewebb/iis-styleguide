cec260d8f4a9baac7967b2c71749fa7759faadae
Tobias Bleckert <tobias.bleckert@iis.se>
2019-01-29 16:06:33 +0100

    Observe dom changes

--- src/organisms/accordion/accordion.js
+++ src/organisms/accordion/accordion.js
@@ -2,18 +2,57 @@ require('van11y-accessible-accordion-aria');
 
 const className = 'o-accordion';
 const accordionElement = document.querySelector(`.js-${className}`);
+let oAccordion = null;
 
-if (accordionElement) {
-	const namespace = getComputedStyle(accordionElement, ':before').content.replace(/["']/g, '');
+function attachAccordion() {
+	if (!oAccordion) {
+		const namespace = getComputedStyle(accordionElement, ':before').content.replace(/["']/g, '');
 
-	const oAccordion = window.van11yAccessibleAccordionAria({
-		ACCORDION_PREFIX_IDS: className,
-		ACCORDION_JS: `js-${className}`,
-		ACCORDION_STYLE: `${namespace}${className}`,
-		ACCORDION_TITLE_STYLE: `${namespace}${className}__title`,
-		ACCORDION_HEADER_STYLE: `${namespace}${className}__header`,
-		ACCORDION_PANEL_STYLE: `${namespace}${className}__panel`,
-	});
+		oAccordion = window.van11yAccessibleAccordionAria({
+			ACCORDION_PREFIX_IDS: className,
+			ACCORDION_JS: `js-${className}`,
+			ACCORDION_STYLE: `${namespace}${className}`,
+			ACCORDION_TITLE_STYLE: `${namespace}${className}__title`,
+			ACCORDION_HEADER_STYLE: `${namespace}${className}__header`,
+			ACCORDION_PANEL_STYLE: `${namespace}${className}__panel`,
+		});
+	}
 
 	oAccordion.attach();
 }
+
+if (accordionElement) {
+	attachAccordion();
+}
+
+const debounce = (func, delay) => {
+	let inDebounce;
+
+	return (...args) => {
+		const context = this;
+		clearTimeout(inDebounce);
+		inDebounce = setTimeout(() => func.apply(context, args), delay);
+	};
+};
+
+const checkElements = debounce(() => {
+	const accordionElementFresh = document.querySelector(`.js-${className}`);
+
+	if (accordionElementFresh) {
+		attachAccordion();
+	}
+}, 50);
+
+const observeHandler = (mutationsList) => {
+	mutationsList.forEach((mutation) => {
+		if (mutation.type !== 'childList') {
+			return;
+		}
+
+		checkElements();
+	});
+};
+
+const observer = new MutationObserver(observeHandler);
+
+observer.observe(document.body, { childList: true, subtree: true });
