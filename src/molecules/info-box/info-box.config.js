5756486cd5ecf10342da8a2935605fbf4aa360b8
rhedman <robert.hedman@creuna.se>
2019-02-13 10:01:36 +0100

    added page example for internet guides

--- src/molecules/info-box/info-box.config.js
+++ src/molecules/info-box/info-box.config.js
@@ -2,6 +2,15 @@ module.exports = {
 	status: 'wip',
 
 	context: {
-		text: 'Sed malesuada quis risus eget malesuada. <strong>Vivamus posuere</strong>, erat sit amet vehicula suscipit, nisi arcu euismod sem, id tempus justo ligula sit amet turpis. <a href="#">Sed faucibus purus</a> sed neque pulvinar, ac faucibus leo tristique.'
-	}
+		text: '<ul><li>Nyttan med öppna licenser</li><li>Att skilja på olika sorters licenser</li><li>Om företagande och öppen källkod</li><li>Hur du använder licenserna i praktiken</li><li>Klargör vilka marknader du riktar dig till och registrera relevanta domäner.</li></ul>'
+	},
+	variants: [
+		{
+			name: 'Big',
+			context: {
+				text: '<p>Sed malesuada quis risus eget malesuada. <strong>Vivamus posuere</strong>, erat sit amet vehicula suscipit, nisi arcu euismod sem, id tempus justo ligula sit amet turpis. <a href="#">Sed faucibus purus</a> sed neque pulvinar, ac faucibus leo tristique.</p>',
+				modifiers: ['big']
+			}
+		}
+	]
 }
