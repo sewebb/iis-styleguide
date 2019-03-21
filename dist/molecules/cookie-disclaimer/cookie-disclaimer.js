0d0143c21d50ef765c607dede7c17df35001fd27
Robert Hedman <robert.hedman@internetstiftelsen.se>
2019-03-21 08:25:13 +0100

    added resourses to git

---
+++ dist/molecules/cookie-disclaimer/cookie-disclaimer.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var cookieBar = document.querySelector('.m-cookie-disclaimer');
+var visibleClass = 'is-visible';
+var cookieName = 'internetstiftelsen-cookie-consent';
+var acceptButton = document.getElementById('js-accept-cookies');
+// const declineButton = document.getElementById('js-decline-cookies');
+
+function setCookie(name, value, days) {
+	var d = new Date();
+	d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days);
+	document.cookie = name + '=' + value + ';path=/;expires=' + d.toGMTString();
+}
+
+function getCookie(name) {
+	var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+	return v ? v[2] : null;
+}
+
+// No cookie set? Show cookie disclaimer bar
+if (!getCookie(cookieName)) {
+	if (cookieBar) {
+		cookieBar.classList.add(visibleClass);
+	}
+}
+
+function acceptCookies() {
+	setCookie(cookieName, 'YES', 365);
+	if (cookieBar) {
+		cookieBar.classList.remove(visibleClass);
+	}
+}
+
+if (acceptButton) {
+	acceptButton.addEventListener('click', acceptCookies);
+}
+
+// ## Don't use this for now
+
+// function deleteCookie(name) { setCookie(name, '', -1); }
+
+// function declineCookies() {
+// 	deleteCookie(cookieName); // Delete cookie if set before
+// 	if (cookieBar) {
+// 		cookieBar.classList.remove(visibleClass);
+// 	}
+// }
+
+
+// if (declineButton) {
+// 	declineButton.addEventListener('click', declineCookies);
+// }
