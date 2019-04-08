87f006271889db45e2abd3cc639c91afa3e36cf1
Robert Hedman <robert.hedman@internetstiftelsen.se>
2019-04-08 13:27:10 +0200

    more customization for isolating footer styles

--- src/organisms/footer/footer.config.js
+++ src/organisms/footer/footer.config.js
@@ -3,9 +3,9 @@ module.exports = {
 
 	context: {
 		id: '',
-		footer_text: '<p><a href="https://internetstiftelsen.se">Internetstiftelsen</a> verkar för ett internet som bidrar positivt till människan och samhället. Vi är en oberoende organisation som säkerställer en stark och säker infrastruktur för internet i Sverige. Vårt mål är att alla ska våga och kunna använda internet. Vi finns i Sverige och ansvarar för den svenska toppdomänen .se och driften av toppdomänen .nu.</p>',
-		contact_1: '<ul class="u-list-clean"><li>Internetstiftelsen i Sverige</li><li>Hammarby Kaj 10D</li><li>box 92073</li><li>120 07 Stockholm</li></ul>',
-		contact_2: '<ul class="u-list-clean"><li>E-post: <a href="mailto:info@internetstiftelsen.se">info@internetstiftelsen.se</a></li><li>Telefon: <a href="tel:084523500">08 452 35 00</a></li><li>Fax: 08 452 35 00</li><li>Org.nr 802405-0190</li></ul>',
+		footer_text: '<p class="o-footer__paragraph"><a class="o-footer__link" href="https://internetstiftelsen.se">Internetstiftelsen</a> verkar för ett internet som bidrar positivt till människan och samhället. Vi är en oberoende organisation som säkerställer en stark och säker infrastruktur för internet i Sverige. Vårt mål är att alla ska våga och kunna använda internet. Vi finns i Sverige och ansvarar för den svenska toppdomänen .se och driften av toppdomänen .nu.</p>',
+		contact_1: '<ul class="u-list-clean o-footer__contact__address"><li class="o-footer__contact__address__li">Internetstiftelsen i Sverige</li><li class="o-footer__contact__address__li">Hammarby Kaj 10D</li><li class="o-footer__contact__address__li">box 92073</li><li class="o-footer__contact__address__li">120 07 Stockholm</li></ul>',
+		contact_2: '<ul class="u-list-clean o-footer__contact__address"><li class="o-footer__contact__address__li">E-post: <a class="o-footer__link" href="mailto:info@internetstiftelsen.se">info@internetstiftelsen.se</a></li><li class="o-footer__contact__address__li">Telefon: <a class="o-footer__link" href="tel:084523500">08 452 35 00</a></li><li class="o-footer__contact__address__li">Fax: 08 452 35 00</li><li class="o-footer__contact__address__li">Org.nr 802405-0190</li></ul>',
 		ISO_link: 'https://internetstiftelsen.se/docs/Certifikat_27001_UKAS_sv.pdf',
 		ISO_image: 'https://internetstiftelsen.se/app/themes/internetstiftelsen/images/ISO_27001_2013_black_TM.svg',
 		ISO_text: 'Certifierade enligt ISO/IEC<br>27001:2013',
