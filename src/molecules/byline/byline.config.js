061e4abf6d099943535fbb6b174636fb84507b65
Robert Hedman <robert.hedman@internetstiftelsen.se>
2019-05-14 10:38:09 +0200

    fixed typo

--- src/molecules/byline/byline.config.js
+++ src/molecules/byline/byline.config.js
@@ -7,6 +7,6 @@ module.exports = {
 		archive_text: 'Visa alla artiklar från skribenten',
 		author_email: false,
 		author_image: '/assets/images/author.png',
-		author_biography: 'Webbfantast som ansvarar för digital strategi på internetstiftelsen. Pennan har jobbat med webben sedan 1994 och har sedan dess drivit egen byrå, jibbat på flera olika communities samt jobbat på kommunikationsbyrå.'
+		author_biography: 'Webbfantast som ansvarar för digital strategi på internetstiftelsen. Pennan har jobbat med webben sedan 1994 och har sedan dess drivit egen byrå, jobbat på flera olika communities samt jobbat på kommunikationsbyrå.'
 	}
 }
