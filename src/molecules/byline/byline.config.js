933c93554af511d46e720248bfa262fe3ba23cdf
Robert Hedman <robert.hedman@internetstiftelsen.se>
2019-05-14 10:36:49 +0200

    byline changes

--- src/molecules/byline/byline.config.js
+++ src/molecules/byline/byline.config.js
@@ -5,7 +5,7 @@ module.exports = {
 		author_name: 'Pennan Modemberg',
 		author_archive: '',
 		archive_text: 'Visa alla artiklar från skribenten',
-		author_email: 'pennan.modemberg@internetstiftelsen.se',
+		author_email: false,
 		author_image: '/assets/images/author.png',
 		author_biography: 'Webbfantast som ansvarar för digital strategi på internetstiftelsen. Pennan har jobbat med webben sedan 1994 och har sedan dess drivit egen byrå, jibbat på flera olika communities samt jobbat på kommunikationsbyrå.'
 	}
