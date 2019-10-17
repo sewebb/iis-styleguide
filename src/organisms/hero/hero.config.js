66dcfa28d4c801d9ba5e2b61aba0b0e5241e4745
Robert Hedman <robert.hedman@internetstiftelsen.se>
2019-10-17 10:29:14 +0200

    text size fixes in hero

--- src/organisms/hero/hero.config.js
+++ src/organisms/hero/hero.config.js
@@ -2,17 +2,27 @@ module.exports = {
 	status: 'ready',
 
 	context: {
-		heading: 'There is nothing quite so permanent as a quick fix',
-		text: 'Internetstiftelsen lanserar ett nytt innovativt verktyg för att avslöja trollen på internet. Och nu behöver vi behöver din hjälp att jaga trollen.',
+		heading: 'Meningsfull tid i rapporten Svenskarna och internet 2019',
 		has_image: true,
-		has_radius: false
+		has_radius: false,
+		limited_width: false
 	},
 	variants: [
+		{
+			name: 'Wrapped',
+			context: {
+				no_image_class: 'o-hero--border-radius',
+				limited_width: true,
+				has_radius: true,
+				text: 'Nästan alla i Sverige använder internet – men hur meningsfull är tiden vi spenderar där? I rapporten Svenskarna och internet 2019 frågar vi för första gången om meningsfullhet, och det syns stora skillnader mellan olika aktiviteter. Tid på sociala medier tycker bara var fjärde är meningsfull.',
+			}
+		},
 		{
 			name: 'No image',
 			context: {
 				no_image_class: 'o-hero--no-image',
-				has_image: false
+				has_image: false,
+				text: 'Nästan alla i Sverige använder internet – men hur meningsfull är tiden vi spenderar där? I rapporten Svenskarna och internet 2019 frågar vi för första gången om meningsfullhet, och det syns stora skillnader mellan olika aktiviteter. Tid på sociala medier tycker bara var fjärde är meningsfull.',
 			}
 		},
 		{
