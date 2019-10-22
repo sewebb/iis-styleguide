f26b764011e4749ee63863afc424f9082415e0a4
Robert Hedman <robert.hedman@internetstiftelsen.se>
2019-10-22 14:39:30 +0200

    compile js

--- dist/molecules/cookie-disclaimer/cookie-disclaimer.js
+++ dist/molecules/cookie-disclaimer/cookie-disclaimer.js
@@ -6,10 +6,19 @@ var cookieName = 'internetstiftelsen-cookie-consent';
 var acceptButton = document.getElementById('js-accept-cookies');
 // const declineButton = document.getElementById('js-decline-cookies');
 
+function isHttps() {
+	return document.location.protocol === 'https:';
+}
+
 function setCookie(name, value, days) {
 	var d = new Date();
 	d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days);
-	document.cookie = name + '=' + value + ';path=/;SameSite=Strict;Secure;expires=' + d.toGMTString();
+
+	if (isHttps) {
+		document.cookie = name + '=' + value + ';path=/;SameSite=Strict;Secure;expires=' + d.toGMTString();
+	} else {
+		document.cookie = name + '=' + value + ';path=/;SameSite=Strict;expires=' + d.toGMTString();
+	}
 }
 
 function getCookie(name) {
