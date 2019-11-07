3b9795cd63c86ffe88ff1ecc8e95c1e7f98496e7
Robert Hedman <robert.hedman@internetstiftelsen.se>
2019-11-07 15:41:55 +0100

    check for cookie support in cookie disclaimer

--- src/molecules/cookie-disclaimer/cookie-disclaimer.js
+++ src/molecules/cookie-disclaimer/cookie-disclaimer.js
@@ -1,10 +1,26 @@
 const cookieBar = document.querySelector('.js-cookie-disclaimer');
 const visibleClass = 'is-visible';
 const cookieName = 'internetstiftelsen-cookie-consent';
+const testCookieSupport = 'Cookies are enabled';
 const acceptButton = document.getElementById('js-accept-cookies');
 const currentProtocol = document.location.protocol;
+let { cookieEnabled } = navigator.cookieEnabled;
 
+// Cookies are disabled
+function showCookieFail() {
+	console.warn('Cookies are disabled, engage tinfoil hat mode.');
+}
 
+// Check for cookie support
+(function checkCookieSupport() {
+	if (!cookieEnabled) {
+		document.cookie = `${testCookieSupport}=Yes;path=/;SameSite=Strict;Secure;`;
+		cookieEnabled = document.cookie.indexOf(testCookieSupport) !== -1;
+	}
+	return cookieEnabled || showCookieFail();
+}());
+
+// Set cookie
 function setCookie(name, value, days) {
 	const d = new Date();
 	d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days);
@@ -16,18 +32,20 @@ function setCookie(name, value, days) {
 	}
 }
 
+// Get cookie
 function getCookie(name) {
 	const v = document.cookie.match(`(^|;) ?${name}=([^;]*)(;|$)`);
 	return v ? v[2] : null;
 }
 
 // No cookie set? Show cookie disclaimer bar
-if (!getCookie(cookieName)) {
+if (!getCookie(cookieName) && cookieEnabled) {
 	if (cookieBar) {
 		cookieBar.classList.add(visibleClass);
 	}
 }
 
+// Cookies accepted
 function acceptCookies() {
 	setCookie(cookieName, 'YES', 365);
 	if (cookieBar) {
@@ -35,6 +53,7 @@ function acceptCookies() {
 	}
 }
 
+// Button click
 if (acceptButton) {
 	acceptButton.addEventListener('click', acceptCookies);
 }
