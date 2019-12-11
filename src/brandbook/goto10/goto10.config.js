7ce42da306f6a87e540a897b324b5b86dcb8247c
Robert Hedman <robert.hedman@internetstiftelsen.se>
2019-12-11 15:46:56 +0100

    switched goto10 colors to default color palette

--- src/brandbook/goto10/goto10.config.js
+++ src/brandbook/goto10/goto10.config.js
@@ -1,5 +1,50 @@
 module.exports = {
 	status: 'wip',
 	label: 'Goto 10',
-	title: 'Goto 10'
+	title: 'Goto 10',
+
+	context: {
+		sass: 'SASS',
+		hex: 'HEX',
+		rgb: 'RGB',
+		cmyk: 'CMYK',
+		pms: 'PMS',
+		colors: [
+			{
+				name: 'lemon',
+				title: '$color-lemon',
+				hex: '#ffce2e',
+				rgb: '255 206 46',
+				cmyk: '0 20 86 0',
+				pms: '120U'
+			},
+			{
+				name: 'lemon-light',
+				title: '$color-lemon-light',
+				hex: '#ffe696',
+				rgb: '255 230 150',
+				cmyk: '0 9 50 0',
+				pms: '1215UP',
+				hasBorder: false
+			},
+			{
+				name: 'black',
+				title: '$color-black',
+				hex: '#000000',
+				rgb: '0 0 0',
+				cmyk: '50 50 50 100',
+				pms: 'n/a',
+				hasBorder: false
+			},
+			{
+				name: 'cyberspace',
+				title: '$color-cyberspace',
+				hex: '#1f2a36',
+				rgb: '31 42 54',
+				cmyk: '88 71 52 41',
+				pms: 'PMS 289U',
+				hasBorder: false
+			}
+		]
+	}
 }
