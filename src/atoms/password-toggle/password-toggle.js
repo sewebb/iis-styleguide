d90f3bfcaa3db2d69f2bc4eb8176745619e8b7cf
Robert Hedman <robert.hedman@internetstiftelsen.se>
2020-03-24 16:37:06 +0100

    removed trailing console.log

--- src/atoms/password-toggle/password-toggle.js
+++ src/atoms/password-toggle/password-toggle.js
@@ -2,8 +2,6 @@ const elements = document.querySelectorAll('.js-toggle-input-type');
 
 if (elements) {
 	[].forEach.call(elements, (element) => {
-		console.log(element);
-
 		element.addEventListener('click', () => {
 			const input = element.previousElementSibling;
 			input.type = (input.type === 'password') ? 'text' : 'password';
