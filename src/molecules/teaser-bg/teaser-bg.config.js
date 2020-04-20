4e07795791cac2c4015786fc492f005b09c39588
tania <tania@earthpeople.se>
2020-04-20 13:24:48 +0200

    Change button text

---
+++ src/molecules/teaser-bg/teaser-bg.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	status: 'ready',
+
+	context: {
+		url: 'http://www.iis.se',
+		link_text: 'Nätneutraliteten och den svenska lagstiftningen',
+		background_image: '/assets/images/KPA_5090-5-4.jpg',
+		icon: 'arrow-forwards',
+
+		alt: 'Hands typing on laptop',
+		img_url: '/assets/images/ALX_0876.jpg',
+		title: 'Gymnasieskolor med hög bandbredd har bättre skolresultat',
+		exerpt: 'Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Faucibus a pellentesque sit amet porttitor.',
+		button_text: 'lorem ipsum',
+		is_padded: true,
+		is_wide: true,
+		has_shadow: false,
+		has_button: false,
+		read_time: true
+	}
+}
