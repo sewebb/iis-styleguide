49c14a04d10ebeedf74cf9d59838b48197add983
tania <tania@earthpeople.se>
2020-04-21 08:20:17 +0200

    Name component better

--- src/molecules/teaser-bg/teaser-bg.config.js
+++ src/molecules/teaser-bg/teaser-bg.config.js
@@ -3,14 +3,14 @@ module.exports = {
 
 	context: {
 		url: 'http://www.iis.se',
-		link_text: 'Nätneutraliteten och den svenska lagstiftningen',
+		content_text: 'Nätneutraliteten och den svenska lagstiftningen',
 		background_image: '/assets/images/KPA_5090-5-4.jpg',
 		icon: 'arrow-forwards',
 
 		alt: 'Hands typing on laptop',
 		img_url: '/assets/images/ALX_0876.jpg',
 		title: 'Gymnasieskolor med hög bandbredd har bättre skolresultat',
-		exerpt: 'Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Faucibus a pellentesque sit amet porttitor.',
+		excerpt: 'Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Faucibus a pellentesque sit amet porttitor.',
 		button_text: 'lorem ipsum',
 		is_padded: true,
 		is_wide: true,
