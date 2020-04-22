f8f22f10904aba6d86e174a140d11b659bbb4e98
Pär Thernström <par.thernstrom@gmail.com>
2020-04-22 10:18:17 +0200

    Add features box version with yellow background and larger text

--- src/organisms/features-box/features-box.config.js
+++ src/organisms/features-box/features-box.config.js
@@ -2,12 +2,19 @@ module.exports = {
 	status: "wip",
 	context: {
 		headline: false,
+		large: false,
 	},
 	variants: [
 		{
 			name: "With headline",
 			context: {
 				headline: true,
+			},
+		},
+		{
+			name: "With larger text",
+			context: {
+				large: true,
 			},
 		},
 	],
