42abbbb8403ccb2613a570c451df9638401f079e
tania <tania@earthpeople.se>
2020-04-23 14:55:59 +0200

    Add title and text

---
+++ src/molecules/modal/modal.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	status: 'wip',
+
+	context: {
+		title: 'Du har sparat FooCoding Graduation!',
+		text: 'En påminnelse kommer skickas till din adress <a href="#">15 minuter</a> innan streamen startar.',
+		button_text: 'Stäng'
+	}
+}
