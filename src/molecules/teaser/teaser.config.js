471348886048aab15aeb0974a967dd7b9a37ee61
tania <tania@earthpeople.se>
2020-04-26 17:01:05 +0200

    WIP teaser with button and excerpt variant

--- src/molecules/teaser/teaser.config.js
+++ src/molecules/teaser/teaser.config.js
@@ -10,11 +10,38 @@ module.exports = {
 		alt: 'Hands typing on laptop',
 		img_url: '/assets/images/ALX_0876.jpg',
 		title: 'Gymnasieskolor med hög bandbredd har bättre skolresultat',
-		exerpt: 'Eget nulla <strong>facilisi etiam</strong> dignissim diam quis enim lobortis scelerisque. Faucibus a pellentesque sit amet porttitor.',
+		excerpt: 'Eget nulla <strong>facilisi etiam</strong> dignissim diam quis enim lobortis scelerisque. Faucibus a pellentesque sit amet porttitor.',
 		is_padded: true,
 		is_wide: true,
 		has_shadow: false,
 		has_button: false,
-		read_time: true
-	}
+		read_time: true,
+		role: "default";
+		link: true
+	},
+
+	variants: [
+			{
+		name: 'excerpt-button',
+		context: {
+			url: 'http://www.iis.se',
+			content_text: 'Nätneutraliteten och den svenska lagstiftningen',
+			background_image: '/assets/images/KPA_5090-5-4.jpg',
+			icon: 'arrow-forwards',
+
+			alt: 'Hands typing on laptop',
+			img_url: '/assets/images/ALX_0876.jpg',
+			title: 'Gymnasieskolor med hög bandbredd har bättre skolresultat',
+			excerpt: 'Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Faucibus a pellentesque sit amet porttitor.',
+			button_text: 'lorem ipsum',
+			is_padded: true,
+			is_wide: true,
+			has_shadow: false,
+			has_button: false,
+			read_time: true,
+			role: "excerpt-button",
+			excerpt_button: true
+			}
+		}
+	]
 }
