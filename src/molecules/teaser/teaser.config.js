9a77fd129acc311a49032f77300c5ada90e19e6a
tania <tania@earthpeople.se>
2020-04-27 14:40:29 +0200

    Refactor teaser component

--- src/molecules/teaser/teaser.config.js
+++ src/molecules/teaser/teaser.config.js
@@ -6,7 +6,6 @@ module.exports = {
 		link_text: 'Nätneutraliteten och den svenska lagstiftningen',
 		background_image: '/assets/images/KPA_5090-5-4.jpg',
 		icon: 'arrow-forwards',
-
 		alt: 'Hands typing on laptop',
 		img_url: '/assets/images/ALX_0876.jpg',
 		title: 'Gymnasieskolor med hög bandbredd har bättre skolresultat',
@@ -17,23 +16,22 @@ module.exports = {
 		has_button: false,
 		read_time: true,
 		role: "default",
-		link: true
 	},
 
 	variants: [
 			{
 		name: 'excerpt-button',
 		context: {
-			url: 'http://www.iis.se',
+			url: false,
 			content_text: 'Nätneutraliteten och den svenska lagstiftningen',
 			background_image: '/assets/images/KPA_5090-5-4.jpg',
 			icon: 'arrow-forwards',
-
 			alt: 'Hands typing on laptop',
 			img_url: '/assets/images/ALX_0876.jpg',
 			title: 'Gymnasieskolor med hög bandbredd har bättre skolresultat',
 			excerpt: 'Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Faucibus a pellentesque sit amet porttitor.',
 			button_text: 'lorem ipsum',
+			button_url: 'http://www.iis.se',
 			is_padded: true,
 			is_wide: true,
 			has_shadow: false,
