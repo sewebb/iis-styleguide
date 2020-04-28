8143f16ac1f961758dbeaa5d419efe2c32cac9fe
tania <tania@earthpeople.se>
2020-04-28 13:27:20 +0200

    Create modal with form variant

--- src/molecules/modal/modal.config.js
+++ src/molecules/modal/modal.config.js
@@ -5,5 +5,14 @@ module.exports = {
 		title: 'Du har sparat FooCoding Graduation!',
 		text: 'En påminnelse kommer skickas till din adress <a href="#">15 minuter</a> innan streamen startar.',
 		button_text: 'Stäng'
-	}
+	},
+
+	variants: [
+		{
+		name: 'form',
+		context: {
+			button_text: 'Skapa konto'
+			}
+		}
+	]
 }
