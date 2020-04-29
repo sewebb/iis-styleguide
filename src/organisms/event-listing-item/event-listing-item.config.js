cc280fe6bf87033e77747331203d86f391a1a8e9
Pär Thernström <par.thernstrom@gmail.com>
2020-04-29 10:53:21 +0200

    Rename event listing to event listing item

---
+++ src/organisms/event-listing-item/event-listing-item.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	status: "wip",
+	title: "Event listing",
+	context: {
+		containerClasses: 'background-snow u-border-radius u-b-0 u-b-l-4 u-b-solid',
+		containerPadding: 'u-p-4',
+		borderColor: "ruby",
+	},
+	variants: [
+		{
+			name: "With jade border color",
+			context: {
+				borderColor: "jade",
+			},
+		},
+	],
+};
--- src/organisms/event-listing/event-listing.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	status: "wip",
-	title: "Event listing",
-	context: {},
-};
