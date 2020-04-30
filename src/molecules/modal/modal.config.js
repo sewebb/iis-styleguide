7cdf0531d71730f8be89a4f7af3df5ac14973539
Robert Hedman <robert.hedman@internetstiftelsen.se>
2020-04-30 08:57:03 +0200

    removed small text sized because of accessibility reasons

--- src/molecules/modal/modal.config.js
+++ src/molecules/modal/modal.config.js
@@ -3,7 +3,7 @@ module.exports = {
 
 	context: {
 		title: 'Du har sparat FooCoding Graduation!',
-		text: 'En påminnelse kommer skickas till din adress <a href="#">15 minuter</a> innan streamen startar.',
+		text: 'En påminnelse kommer skickas till din adress <a href="#" class="u-link">15 minuter</a> innan streamen startar.',
 		button_text: 'Stäng'
 	},
 
@@ -15,8 +15,8 @@ module.exports = {
 			button_text: 'Skapa konto',
 			is_form: true,
 			check_text: 'Prenumerera på våra lokala nyhetsbrev:',
-			terms_text: 'Jag godkänner Goto 10:s <a href="#">medlemsvillkor</a> och <a href="#">integritetspolicy</a>.',
-			bottom_text: 'Har du redan ett konto? <a href="#">Logga in</a>'
+			terms_text: 'Jag godkänner Goto 10:s <a href="#" class="u-link">medlemsvillkor</a> och <a href="#" class="u-link">integritetspolicy</a>.',
+			bottom_text: 'Har du redan ett konto? <a href="#" class="u-link">Logga in</a>'
 			}
 		}
 	]
