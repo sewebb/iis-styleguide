1510b905bfb2003e2b4bdce4e1fe65a4f4112076
Tobias Bleckert <hola@tobiasbleckert.se>
2020-05-07 10:59:05 +0200

    Only add listeners if element exists

--- src/molecules/modal/modal.js
+++ src/molecules/modal/modal.js
@@ -2,8 +2,6 @@ const createFocusTrap = require('focus-trap');
 
 const button = document.querySelector('.js-modal-open');
 const modal = document.querySelector('#modal-container');
-
-
 const focusTrapOne = createFocusTrap('#modal-container', {
 	onDeactivate() {
 		modal.classList.add('is-hidden');
@@ -22,14 +20,18 @@ function closeModal() {
 	focusTrapOne.deactivate();
 }
 
-button.addEventListener('click', (e) => {
-	e.preventDefault();
-	openModal();
-});
-
-modal.addEventListener('click', (e) => {
-	if (e.target.classList.contains('js-modal-close')) {
+if (button) {
+	button.addEventListener('click', (e) => {
 		e.preventDefault();
-		closeModal();
-	}
-});
+		openModal();
+	});
+}
+
+if (modal) {
+	modal.addEventListener('click', (e) => {
+		if (e.target.classList.contains('js-modal-close')) {
+			e.preventDefault();
+			closeModal();
+		}
+	});
+}
