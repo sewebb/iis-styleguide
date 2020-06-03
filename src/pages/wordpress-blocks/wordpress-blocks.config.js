f98cf9c982f9dd5a8423e9d919f9e56fcbf659d2
Tobias Bleckert <hola@tobiasbleckert.se>
2020-06-03 13:23:35 +0200

    Added exceptions for grid and hero

--- src/pages/wordpress-blocks/wordpress-blocks.config.js
+++ src/pages/wordpress-blocks/wordpress-blocks.config.js
@@ -2,6 +2,18 @@ module.exports = {
 	status: 'wip',
 
 	context: {
-		
+		alt: 'Hands typing on laptop',
+		img_url: '/assets/images/ALX_0876.jpg',
+		title: 'Gymnasieskolor med hög bandbredd har bättre skolresultat',
+		url: 'http://www.google.se',
+		exerpt: 'Eget nulla <strong>facilisi etiam</strong> dignissim diam quis enim lobortis scelerisque. Faucibus a pellentesque sit amet porttitor.',
+		is_padded: true,
+		is_wide: false,
+		has_shadow: false,
+		has_button: false,
+		read_time: true,
+		has_border: false,
+		has_image: true,
+		has_radius: true
 	}
 }
