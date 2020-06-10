9eb98f6d6e4d42c7e69c41269f91df6308df17db
Robert Hedman <robert.hedman@internetstiftelsen.se>
2020-06-10 15:09:15 +0200

    finished POC for height limiter

--- src/atoms/height-limiter/height-limiter.js
+++ src/atoms/height-limiter/height-limiter.js
@@ -3,17 +3,36 @@ const elements = document.querySelectorAll('.js-height-limit');
 if (elements) {
 	[].forEach.call(elements, (element) => {
 		const height = element.getAttribute('data-height');
-		const innerContainer = element;
-		element.setAttribute('style', `max-height:${height}px;`);
-		element.addEventListener('click', () => {
-			console.log('foo');
+		const innerContainer = element.querySelector('[class*="inner"]');
+		const button = element.querySelector('.js-toggle-height');
+		const buttonText = button.querySelector('span');
+
+		if (element.offsetHeight >= height) {
+			innerContainer.setAttribute('style', `max-height:${height}px;`);
+			innerContainer.classList.add('is-limited');
+			button.classList.remove('is-hidden');
+		} else {
+			innerContainer.removeAttribute('style');
+			innerContainer.classList.remove('is-limited');
+			button.classList.add('is-hidden');
+		}
+
+		button.addEventListener('click', () => {
+			innerContainer.classList.toggle('is-limited');
+			innerContainer.setAttribute('style', (innerContainer.style.maxHeight === `${height}px`) ? 'max-height:none' : `max-height:${height}px`);
+			buttonText.innerText = (buttonText.innerText === 'Visa mer') ? 'Visa mindre' : 'Visa mer';
+			button.classList.toggle('is-clicked');
 		});
 
 		window.addEventListener('resize', () => {
-			if (element.offsetHeight >= height) {
-				element.classList.add('is-limited');
+			if (innerContainer.offsetHeight >= height) {
+				innerContainer.setAttribute('style', `max-height:${height}px;`);
+				innerContainer.classList.add('is-limited');
+				button.classList.remove('is-hidden');
 			} else {
-				element.classList.remove('is-limited');
+				innerContainer.removeAttribute('style');
+				innerContainer.classList.remove('is-limited');
+				button.classList.add('is-hidden');
 			}
 		});
 	});
