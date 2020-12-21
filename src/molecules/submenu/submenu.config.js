16b832e8e0e563765a0e0662dbf0788f0fbf3506
Robert Hedman <robert.hedman@internetstiftelsen.se>
2020-12-21 14:03:09 +0100

    POC for sublevel

--- src/molecules/submenu/submenu.config.js
+++ src/molecules/submenu/submenu.config.js
@@ -5,5 +5,13 @@ module.exports = {
 		background_color: '#ffce2e',
 		icon_background_color: '#ffce2e',
 		icon_hover_color: '#1f2a36',
-	}
+	},
+	variants: [
+		{
+			name: 'With sublevel',
+			context: {
+				has_sublevel: true
+			}
+		}
+	]
 }
