78fd4bbe7d8c20a5e6f5b2327c118397444f508b
Robert Hedman <robert.hedman@internetstiftelsen.se>
2021-01-07 10:34:57 +0100

    adjusted margins for info-box to fit the blockifying of sites

--- src/molecules/info-box/info-box.config.js
+++ src/molecules/info-box/info-box.config.js
@@ -9,7 +9,8 @@ module.exports = {
 			name: 'Big',
 			context: {
 				text: '<p>Sed malesuada quis risus eget malesuada. <strong>Vivamus posuere</strong>, erat sit amet vehicula suscipit, nisi arcu euismod sem, id tempus justo ligula sit amet turpis. <a href="#">Sed faucibus purus</a> sed neque pulvinar, ac faucibus leo tristique.</p>',
-				modifiers: ['big']
+				modifiers: ['big'],
+				additional_classes: false
 			}
 		}
 	]
