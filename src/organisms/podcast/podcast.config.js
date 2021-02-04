43dd2b2a4c09a4af539c7c1b8c63dbe6776443e5
Robert Hedman <robert.hedman@internetstiftelsen.se>
2021-02-04 14:15:31 +0100

    fetch different episodes in episode list

--- src/organisms/podcast/podcast.config.js
+++ src/organisms/podcast/podcast.config.js
@@ -13,6 +13,7 @@ module.exports = {
 		duration: '53:04',
 		has_modifier: false,
 		is_single_episode: true,
+		episodeImage: false
 	},
 
 	variants: [
@@ -37,13 +38,28 @@ module.exports = {
 				modifiers: ['list'],
 				episodes: [
 					{
-
+						title: 'Svenskarna och internet 2020',
+						src: 'https://traffic.libsyn.com/secure/internetpodden/20201215-svenskarna-och-internet-2020.mp3?dest-id=1534313',
+						episodeImage: 'https://ssl-static.libsyn.com/p/assets/a/9/6/5/a9653e010511f97e/internetpodden-logo.png',
+						description: 'Årets högtidsstund för alla som älskar internetstatistik är här! Vår rapport Svenskarna och internet har utökats inför ett speciellt år med extra frågor kring Coronapandemin. Jannike Tillå, Måns Jonasson och Jenny Andersson från Internetstiftelsen diskuterar insikterna från årets rapport.',
+						duration: '53:04',
+						date: '15 december, 2020'
 					},
 					{
-
+						title: 'David Rowan på Internetdagarna 2019',
+						src: 'https://traffic.libsyn.com/secure/internetpodden/Internetpodden_avsnitt5.mp3?dest-id=1534313',
+						episodeImage: 'https://ssl-static.libsyn.com/p/assets/3/6/a/2/36a281563e8e08b8/Internetpodden_avsnitt5.jpg',
+						description: 'Vi fortsätter våra samtal med huvudtalarna från årets Internetdagarna. I det här avsnittet samtalar Internetstiftelsens Måns Jonasson med David Rowan, tidigare chefredaktör för teknikmagasinet Wired. Rowan höll en hyllad presentation på temat innovationer, och visade upp en del inte fullt så innovativa uppfinningar. Vad är egentligen riktig innovation?',
+						duration: '17:12',
+						date: '30 december, 2019'
 					},
 					{
-
+						title: 'Jenny Radcliffe på Internetdagarna 2019',
+						src: 'https://traffic.libsyn.com/secure/internetpodden/internetpodden_avsnitt3.mp3?dest-id=1534313',
+						episodeImage: 'https://ssl-static.libsyn.com/p/assets/7/4/b/7/74b768ea833215f0/internetpodden_avsnitt3.jpg',
+						description: 'Vi fortsätter våra samtal med huvudtalarna från årets Internetdagarna. I det här avsnittet samtalar Internetstiftelsens Isadora Hellegren med Jenny Radcliffe, känd som "The People Hacker". Hon tar sig in i vilken byggnad som helst genom konsten att förhandla och övertala, och utnyttja säkerhetssystemens svagaste länk; människan.',
+						duration: '13:46',
+						date: '15 december, 2019'
 					}
 				]
 			}
