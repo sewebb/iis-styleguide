331d9575a2293f176d4d833dfb3484930802e232
Robert Hedman <robert.hedman@internetstiftelsen.se>
2021-02-16 15:39:49 +0100

    fixed config bug in info-box

--- src/molecules/info-box/info-box.config.js
+++ src/molecules/info-box/info-box.config.js
@@ -11,7 +11,9 @@ module.exports = {
 				text: '<p>Sed malesuada quis risus eget malesuada. <strong>Vivamus posuere</strong>, erat sit amet vehicula suscipit, nisi arcu euismod sem, id tempus justo ligula sit amet turpis. <a href="#">Sed faucibus purus</a> sed neque pulvinar, ac faucibus leo tristique.</p>',
 				modifiers: ['big'],
 				additional_classes: false
-			},
+			}
+		},
+		{
 			name: 'Basic',
 			context: {
 				text: '<p>Sed malesuada quis risus eget malesuada. <strong>Vivamus posuere</strong>, erat sit amet vehicula suscipit, nisi arcu euismod sem, id tempus justo ligula sit amet turpis. <a href="#">Sed faucibus purus</a> sed neque pulvinar, ac faucibus leo tristique.</p>',
