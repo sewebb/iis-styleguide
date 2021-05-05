c691e8273be621867898ea0c311aca1179a84e56
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2021-05-05 10:11:01 +0200

    First stab at controllable youtube

---
+++ src/assets/js/youtube.js
@@ -0,0 +1,68 @@
+function loadYoutubeAPI() {
+	const tag = document.createElement('script');
+	const firstScript = document.getElementsByTagName('script')[0];
+
+	tag.src = 'https://www.youtube.com/iframe_api';
+
+	firstScript.parentNode.insertBefore(tag, firstScript);
+}
+
+function onPlayerReady(el, e) {
+	el.addEventListener('click', () => {
+		e.target.playVideo();
+	});
+}
+
+function onPlayerStateChange(el, e) {
+	if (e.data === YT.PlayerState.PLAYING) {
+		el.getElementsByTagName('img')[0].style.opacity = '0';
+		el.getElementsByTagName('svg')[0].style.opacity = '0';
+	}
+}
+
+function createCover(el, id) {
+	const url = `https://i3.ytimg.com/vi/${id}/maxresdefault.jpg`;
+	const img = document.createElement('img');
+
+	el.appendChild(img);
+
+	img.src = url;
+}
+
+function setupYoutubePlayer(el) {
+	const playerEl = document.createElement('div');
+	const id = el.getAttribute('data-youtube');
+
+	if (!el.getElementsByTagName('img').length) {
+		createCover(el, id);
+	}
+
+	el.appendChild(playerEl);
+
+	const player = new YT.Player(playerEl, {
+		height: '100%',
+		width: '100%',
+		videoId: id,
+		playerVars: {
+			playsinline: 1,
+		},
+		events: {
+			onReady: (e) => onPlayerReady(el, e),
+			onStateChange: (e) => onPlayerStateChange(el, e),
+		},
+	});
+
+	el.youtube = player;
+}
+
+window.onYouTubeIframeAPIReady = () => {
+	const players = document.querySelectorAll('[data-youtube]');
+
+	if (!players.length) {
+		return;
+	}
+
+	[].forEach.call(players, setupYoutubePlayer);
+};
+
+loadYoutubeAPI();
