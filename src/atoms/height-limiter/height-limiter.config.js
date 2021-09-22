52e12bdb357893c7b9d9ac92742006c293e70854
Robert Hedman <robert.hedman@internetstiftelsen.se>
2021-09-22 08:57:07 +0200

    update states of finished components

--- src/atoms/height-limiter/height-limiter.config.js
+++ src/atoms/height-limiter/height-limiter.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	status: 'wip',
+	status: 'ready',
 
 	context: {
 		title: 'Height limiter',
--- src/molecules/submenu/submenu.config.js
+++ src/molecules/submenu/submenu.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	status: 'wip',
+	status: 'ready',
 
 	context: {
 		background_color: '#ffce2e',
