f3c730a4c738f1236baed33bdf5bd9f57905a309
Robert Hedman <robert.hedman@internetstiftelsen.se>
2021-09-28 11:21:12 +0200

    added support for multiple gliders on the same page

--- src/molecules/glider/glider.js
+++ src/molecules/glider/glider.js
@@ -1,41 +1,44 @@
 import Glider from 'glider-js';
 
-const gliderElement = document.querySelector('.js-glider');
+const gliderWrappers = document.querySelectorAll('.glider-contain');
 
-if (gliderElement) {
-	const glider = new Glider(gliderElement, {
-		scrollLock: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		dots: '.glider-dots',
-		arrows: {
-			prev: '.js-glider-prev',
-			next: '.js-glider-next',
-		},
-		responsive: [
-			{
-				breakpoint: 961,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3,
-				},
+if (gliderWrappers) {
+	[].forEach.call(gliderWrappers, (gliderWrapper) => {
+		const gliderElement = gliderWrapper.querySelector('.js-glider');
+
+		const glider = new Glider(gliderElement, {
+			scrollLock: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			dots: gliderElement.querySelector('.glider-dots'),
+			arrows: {
+				prev: gliderWrapper.querySelector('.js-glider-prev'),
+				next: gliderWrapper.querySelector('.js-glider-next'),
 			},
-			{
-				breakpoint: 769,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 2,
+			responsive: [
+				{
+					breakpoint: 961,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 3,
+					},
 				},
-			},
-			{
-				breakpoint: 469,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1,
+				{
+					breakpoint: 769,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2,
+					},
 				},
-			},
-		],
+				{
+					breakpoint: 469,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+					},
+				},
+			],
+		});
+		module.exports = glider;
 	});
-
-	module.exports = glider;
 }
