d125a9150e95c0d5cd98d5bf92c8cb160817539a
Robert Hedman <robert.hedman@internetstiftelsen.se>
2021-12-02 14:53:24 +0100

    update texts

--- src/organisms/footer/footer.config.js
+++ src/organisms/footer/footer.config.js
@@ -21,6 +21,8 @@ module.exports = {
 		site_heading_5: 'Internetmuseum',
 		site_description_5: 'Internetmuseum är ett digitalt museum som byggts, och kureras av Internetstiftelsen',
 		site_heading_6: 'Digitala lektioner',
-		site_description_6: 'Digitala lektioner är en öppen digital lärresurs med färdiga lektioner för alla stadier i grundskolan.',
+		site_description_6: 'Öppen digital lärresurs med färdiga lektioner för alla stadier i grundskolan',
+		site_heading_7: 'Internetkunskap',
+		site_description_7: 'Samlad kunskap som hjälper dig att bli en säker och medveten internetanvändare'
 	}
 }
