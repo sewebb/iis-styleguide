28dd16c21b9b9ccd2bfdf0a3edffc883e1a93820
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2022-06-01 10:13:17 +0200

    Function to re-setup youtube players for specified container
    (cherry picked from commit e54686a8a2c59f3ca65bd79cc9f9de78b0ba1582)

--- src/assets/js/youtube.js
+++ src/assets/js/youtube.js
@@ -7,10 +7,8 @@ function loadYoutubeAPI() {
 	firstScript.parentNode.insertBefore(tag, firstScript);
 }
 
-function onPlayerReady(el, e) {
-	el.addEventListener('click', () => {
-		e.target.playVideo();
-	});
+function onPlayerReady(el) {
+	el.setAttribute('data-youtube-ready', 'true');
 }
 
 function onPlayerStateChange(el, e) {
@@ -30,16 +28,28 @@ function createCover(el, id) {
 }
 
 function setupYoutubePlayer(el) {
-	const playerEl = document.createElement('div');
+	let playerEl;
 	const id = el.getAttribute('data-youtube');
+
+	if (el.youtube) {
+		playerEl = el.querySelector('[data-youtube-container]');
+
+		playerEl.parentNode.removeChild(playerEl);
+		el.youtube.destroy();
+		el.youtube = null;
+		el.removeAttribute('data-youtube-ready');
+	}
+
+	playerEl = document.createElement('div');
 
 	if (!el.getElementsByTagName('img').length) {
 		createCover(el, id);
 	}
 
+	playerEl.setAttribute('data-youtube-container', true);
 	el.appendChild(playerEl);
 
-	const player = new YT.Player(playerEl, {
+	el.youtube = new YT.Player(playerEl, {
 		height: '100%',
 		width: '100%',
 		videoId: id,
@@ -53,18 +63,33 @@ function setupYoutubePlayer(el) {
 			onStateChange: (e) => onPlayerStateChange(el, e),
 		},
 	});
-
-	el.youtube = player;
 }
 
-window.onYouTubeIframeAPIReady = () => {
-	const players = document.querySelectorAll('[data-youtube]');
+function delegateClick(e) {
+	const el = e.target.closest('[data-youtube]');
+
+	if (!el || !el.youtube) {
+		return;
+	}
+
+	el.youtube.playVideo();
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export function setupPlayers(container) {
+	const players = container.querySelectorAll('[data-youtube]');
 
 	if (!players.length) {
 		return;
 	}
 
 	[].forEach.call(players, setupYoutubePlayer);
+}
+
+window.onYouTubeIframeAPIReady = () => {
+	setupPlayers(document);
+
+	document.body.addEventListener('click', delegateClick);
 };
 
 loadYoutubeAPI();
