85c53617a68cc72cb0a5e9b4cbbee9a0426552f0
Robert Hedman <robert.hedman@internetstiftelsen.se>
2022-07-06 09:12:45 +0200

    extend tag color variations

--- src/atoms/tag/tag.config.js
+++ src/atoms/tag/tag.config.js
@@ -15,21 +15,59 @@ module.exports = {
 			}
 		},
 		{
-			name: 'Background',
+			name: 'Background variation',
 			context: {
 				text: 'background',
 				href: '#',
 				is_light: false,
-				background_color: 'jade'
+				border: true,
+				border_color: 'ocean',
+				background_color: 'ocean',
+				hover_color: 'light',
+				color: 'ocean'
 			}
 		},
 		{
-			name: 'Combined',
+			name: 'Border variation',
 			context: {
-				text: 'combined',
+				text: 'border',
 				href: '#',
-				is_light: true,
-				background_color: 'ruby'
+				is_light: false,
+				border: true,
+				border_color: 'sandstone',
+				background_color: 'sandstone-light',
+				hover_color: 'light',
+				color: 'sandstone'
+			}
+		},
+		{
+			name: 'With right icon',
+			context: {
+				text: 'with right icon',
+				href: '#',
+				icon: 'pin',
+				icon_align_right: true,
+				is_light: false,
+				border: true,
+				border_color: 'lemon',
+				background_color: 'lemon-light',
+				hover_color: 'dark',
+				color: 'lemon'
+			}
+		},
+		{
+			name: 'With left icon',
+			context: {
+				text: 'with left icon',
+				href: '#',
+				icon: 'pin',
+				icon_align_left: true,
+				is_light: false,
+				border: true,
+				border_color: 'jade',
+				background_color: 'jade-light',
+				hover_color: 'dark',
+				color: 'jade'
 			}
 		}
 	]
