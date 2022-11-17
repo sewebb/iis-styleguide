d677809918fb90c6ac00e72e8382c7e6235acb12
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2022-11-17 08:16:37 +0100

    Export function to recache parallax elements

--- dist/assets/js/parallax.js
+++ dist/assets/js/parallax.js
@@ -1,14 +1,29 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.cache = cache;
 function isInView(el) {
 	var box = el.getBoundingClientRect();
 	return box.top < window.innerHeight && box.bottom >= 0;
 }
 
-var parallaxes = document.querySelectorAll('.js-parallax');
+var parallaxes = [];
+
+// eslint-disable-next-line import/prefer-default-export
+function cache() {
+	parallaxes = document.querySelectorAll('.js-parallax');
+}
+
+cache();
 
 window.addEventListener('scroll', function () {
 	[].forEach.call(parallaxes, function (parallax) {
+		if (parallax.classList.contains('animate')) {
+			return;
+		}
+
 		var visible = isInView(parallax);
 		if (visible) {
 			parallax.classList.add('animate');
