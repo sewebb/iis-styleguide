39e7aa5a3b0066e20fcc53b1129b508c7c735ea9
Robert Hedman <robert.hedman@internetstiftelsen.se>
2023-02-15 08:09:50 +0100

    Prototype for report page

---
+++ src/atoms/hr/hr.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+	status: 'ready',
+
+	context: {
+		color: null,
+		modifier: null,
+	},
+	variants: [
+		{
+			name: 'alignfull',
+			context: {
+				class: 'alignfull',
+				color: 'color-cyberspace',
+				modifier: 'alignfull',
+			}
+		},
+		{
+			name: 'alignwide',
+			context: {
+				class: 'alignwide',
+				color: 'color-cyberspace',
+				modifier: 'alignwide',
+			}
+		},
+		{
+			name: 'aligncenter',
+			context: {
+				class: null,
+				color: 'color-cyberspace',
+				modifier: 'aligncenter',
+			}
+		},
+		{
+			name: 'alignleft',
+			context: {
+				class: null,
+				color: 'has-cyberspace-color',
+				modifier: 'alignleft',
+			}
+		}
+	]
+}
