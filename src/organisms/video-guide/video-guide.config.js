3a3953c03401901bf9683cbb689afbca20f857fd
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2023-03-08 11:25:37 +0100

    Separate posts and images and keep last active post until change

--- src/organisms/video-guide/video-guide.config.js
+++ src/organisms/video-guide/video-guide.config.js
@@ -2,8 +2,8 @@ module.exports = {
 	status: 'prototype',
 
 	context: {
-		videoSrc: 'https://ny.internetmuseum.se/app/uploads/2023/01/guidad-tur-nar-internet-kom-till-svenska-hem.mp4',
-		videoSrc2: 'https://ny.internetmuseum.se/app/uploads/2023/01/guidad-tur-nar-internet-kom-till-svenska-hem-vp9-chrome.webm',
+		videoSrc: 'https://internetmuseum.se/app/uploads/2023/01/guidad-tur-nar-internet-kom-till-svenska-hem.mp4',
+		videoSrc2: 'https://internetmuseum.se/app/uploads/2023/01/guidad-tur-nar-internet-kom-till-svenska-hem-vp9-chrome.webm',
 
 		variants: [
 			{
