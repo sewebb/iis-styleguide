5cadde34ed0eb1003a150e8c5261bda3732a15e0
Robert Hedman <robert.hedman@internetstiftelsen.se>
2023-10-31 12:36:34 +0100

    continue layout for BBK

--- src/pages/bredbandskollen/bredbandskollen.config.js
+++ src/pages/bredbandskollen/bredbandskollen.config.js
@@ -5,5 +5,96 @@ module.exports = {
 		hidden: 'true',
 		id: true,
 		addidional_classes: false,
+		columns: [
+			{
+				headline: 'Om Bredbandskollen',
+				topics: [
+					{
+						topic: 'Mer om Bredbandskollen',
+						items: [
+							{
+								item: 'Appar för'
+							},
+							{
+								item: 'IOS och Android'
+							},
+							{
+								item: 'Värdera din uppkoppling'
+							},
+							{
+								item: 'Bredbandskollen över IPv6'
+							},
+							{
+								item: 'Bredbandskollen CLI'
+							},
+							{
+								item: 'Appar och behörighet'
+							},
+							{
+								item: 'Problem med webbläsartillägg'
+							},
+							{
+								item: 'Mätning över http och websockets'
+							}
+
+
+						]
+					}
+				],
+			},
+			{
+				headline: 'Hjälp',
+				topics: [
+					{
+						topic: '',
+						items: [
+							{
+								item: 'Frågor och svar'
+							},
+							{
+								item: 'Surfa mobilt'
+							},
+							{
+								item: 'Surfa trådlöst'
+							},
+
+						]
+					}
+				],
+			},
+			{
+				headline: 'English',
+				topics: [
+					{
+						topic: 'About Bredbandskollen',
+						items: [
+							{
+								item: 'More about Bredbandskollen'
+							},
+							{
+								item: 'Contact'
+							}
+
+						]
+					}
+				],
+			},
+			{
+				headline: 'Kontakt',
+				topics: [
+					{
+						topic: '',
+						items: [
+							{
+								item: 'Avtalsvillkor',
+							},
+							{
+								item: 'Integritetspolicy',
+							}
+						]
+					}
+				],
+			},
+		]
 	}
 }
