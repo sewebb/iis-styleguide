b03e95bcd374f6a6f490f165de245d35549123b4
Robert Hedman <robert.hedman@internetstiftelsen.se>
2023-11-13 13:33:59 +0100

    added Range Component

---
+++ src/atoms/range/range.js
@@ -0,0 +1,22 @@
+const ranges = document.querySelectorAll('.js-range-wrapper');
+function setValue(range, rangeValue) {
+	const val = range.value;
+	const min = range.min ? range.min : 0;
+	const max = range.max ? range.max : 100;
+	const newVal = Number(((val - min) * 100) / (max - min));
+	rangeValue.innerHTML = val;
+
+	// Sorta magic numbers based on size of the native UI thumb
+	rangeValue.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
+}
+
+ranges.forEach((wrap) => {
+	const range = wrap.querySelector('.js-range');
+	const rangeValue = wrap.querySelector('.js-range-value');
+
+	range.addEventListener('input', () => {
+		setValue(range, rangeValue);
+	});
+
+	setValue(range, rangeValue);
+});
