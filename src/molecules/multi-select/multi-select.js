760f6b0308dd1d162a5a83a84a680e87e392e93d
Tobias Bleckert <hola@tobiasbleckert.se>
2024-06-27 11:19:23 +0200

    Multi select refactor

--- src/molecules/multi-select/multi-select.js
+++ src/molecules/multi-select/multi-select.js
@@ -1,134 +1,182 @@
-/* eslint-disable */
-const className = 'm-multi-select';
-const multiSelectElements = document.querySelectorAll(`.js-${className}`);
-let namespace;
+import className from '../../assets/js/className';
 
-if( multiSelectElements.length ) {
-	namespace = getComputedStyle(multiSelectElements[0], ':before').content.replace(/["']/g, '');
-}
+class MultiSelect {
+	constructor(el) {
+		this.element = el;
+		this.baseClassName = 'm-multi-select';
+		this.currentFocus = -1;
+		this.input = this.element.querySelector(`.js-${this.baseClassName}__input`);
+		this.suggestionsBox = this.element.querySelector(`.js-${this.baseClassName}-suggestions-box`);
+		this.selectedItemsList = this.element.querySelector('.js-m-multi-select-selected-items');
+		this.selectedItems = [];
+		this.data = [];
 
-let currentFocus = -1; // Tracks the currently focused item in the suggestions
-
-// Highlight the active (focused) suggestion
-function setActive(items) {
-	if (!items.length) return false;
-	removeActive(items);
-	if (currentFocus >= items.length) currentFocus = 0;
-	if (currentFocus < 0) currentFocus = items.length - 1;
-	items[currentFocus].classList.add('autocomplete-active');
-
-	return items;
-}
-
-// Remove highlighting from all suggestions
-function removeActive(items) {
-	for (let i = 0; i < items.length; i+=1) {
-		items[i].classList.remove('autocomplete-active');
+		this.getData();
+		this.attach();
 	}
-}
 
-// Add a selected item to the list of selected items
-function addSelectedItem(item) {
-	const selectedItemsList = document.querySelector('.js-m-multi-select-selected-items');
-	const newItem = document.createElement('li');
-	newItem.textContent = item + ' ';
-	newItem.classList.add(`${namespace}a-tag`);
-	newItem.classList.add(`${namespace}m-multi-select__tag`);
+	getData() {
+		const id = this.input.getAttribute('data-multi-select-suggestions');
+		const el = document.getElementById(id);
 
-	const removeBtn = document.createElement('button');
-	removeBtn.classList.add(`${namespace}m-multi-select-selected-items__remove-btn`);
-
-	const buttonTextContainer = document.createElement('span');
-	buttonTextContainer.classList.add('u-visuallyhidden');
-	removeBtn.appendChild(buttonTextContainer);
-	buttonTextContainer.textContent ='Ta bort ' + item; // Accessibility label for screen readers
-
-	// Event listener for removing the selected item
-	removeBtn.addEventListener('click', function () {
-		removeItem(newItem, Array.from(selectedItemsList.children).indexOf(newItem));
-	});
-	newItem.appendChild(removeBtn);
-	selectedItemsList.appendChild(newItem);
-}
-
-// Remove an item and manage focus appropriately
-function removeItem(item, index) {
-	const selectedItemsList = document.querySelector('.js-m-multi-select-selected-items');
-	selectedItemsList.removeChild(item);
-
-	let remainingItems = selectedItemsList.getElementsByTagName('div');
-	// Focus management: set focus to the next item, or the search input if no items left
-	if (remainingItems.length > 0) {
-		if (index < remainingItems.length) {
-			remainingItems[index].getElementsByTagName('button')[0].focus();
-		} else {
-			remainingItems[remainingItems.length - 1].getElementsByTagName('button')[0].focus();
-		}
-	} else {
-		multiSelectInput.focus();
-	}
-}
-
-function setup(multiSelectElement) {
-	const multiSelectInput = multiSelectElement.querySelector(`.js-${className}__input`);
-	const suggestionsBox = multiSelectElement.querySelector(`.js-${className}-suggestions-box`);
-	const suggestionsData = multiSelectInput.getAttribute('data-multi-select-suggestions');
-
-	// Event listener for input changes in the search field
-	multiSelectInput.addEventListener('input', function () {
-		const value = this.value;
-		// Clear suggestions if less than 2 characters are typed
-		if (value.length < 2) {
-			suggestionsBox.innerHTML = '';
+		if (!el) {
+			this.data = [];
 			return;
 		}
 
-		// Define JSON
-		let suggestions = document.getElementById(suggestionsData).textContent;
-		suggestions = JSON.parse(suggestions);
+		this.data = JSON.parse(el.textContent);
+	}
 
-		// Filter suggestions based on the input value
-		const filtered = suggestions.filter(item => item.name.toLowerCase().startsWith(value.toLowerCase()));
-		// Populate the suggestions box with the filtered results
-		suggestionsBox.innerHTML = filtered.map(item => `<button class='${namespace}m-multi-select__suggestion-btn' tabindex='0'>${item.name}</button>`).join('');
-		// Reset the current focus
-		currentFocus = -1;
-	});
+	attach() {
+		this.input.addEventListener('input', this.onInput);
+		this.input.addEventListener('keydown', this.onKeyDown);
+		this.suggestionsBox.addEventListener('click', this.onClick);
+	}
 
-	// Event listener for keydown events for navigation and selection in the suggestions box
-	multiSelectInput.addEventListener('keydown', function (e) {
-		let items = suggestionsBox.getElementsByClassName(`${namespace}m-multi-select__suggestion-btn`);
-		// Navigate down in the suggestions list
-		if (e.keyCode == 40) {
-			currentFocus = (currentFocus + 1) % items.length;
-			setActive(items);
-			// Navigate up in the suggestions list
-		} else if (e.keyCode == 38) {
-			currentFocus = (currentFocus - 1 + items.length) % items.length;
-			setActive(items);
-			// Handle Enter key to select a focused item
-		} else if (e.keyCode == 13) {
-			e.preventDefault();
-			if (currentFocus > -1 && items[currentFocus]) {
-				addSelectedItem(items[currentFocus].textContent);
-				suggestionsBox.innerHTML = '';
-				multiSelectInput.value = '';
-				currentFocus = -1;
+	setFocus(index) {
+		this.currentFocus = index;
+	}
+
+	resetFocus() {
+		this.setFocus(-1);
+	}
+
+	clearSuggestions() {
+		this.suggestionsBox.innerHTML = '';
+	}
+
+	filterData(query) {
+		return this.data
+			.filter((item) => item.name.toLowerCase().startsWith(query.toLowerCase()))
+			.filter((item) => !this.selectedItems.includes(item.name));
+	}
+
+	populateSuggestions(suggestions) {
+		const cls = className(`${this.baseClassName}__suggestion-btn`);
+		this.suggestionsBox.innerHTML = suggestions.map((item) => `<button class='${cls}' tabindex='0'>${item.name}</button>`).join('');
+	}
+
+	onInput = () => {
+		const { value } = this.input;
+
+		// Clear suggestions if less than 2 characters are typed
+		if (value.length < 2) {
+			this.clearSuggestions();
+
+			return;
+		}
+
+		const suggestions = this.filterData(value);
+
+		this.populateSuggestions(suggestions);
+		this.resetFocus();
+	}
+
+	removeItem(item, index) {
+		const selectedItemsList = this.element.querySelector('.js-m-multi-select-selected-items');
+		selectedItemsList.removeChild(item);
+
+		const remainingItems = selectedItemsList.getElementsByTagName('div');
+
+		// Focus management: set focus to the next item, or the search input if no items left
+		if (remainingItems.length > 0) {
+			if (index < remainingItems.length) {
+				remainingItems[index].getElementsByTagName('button')[0].focus();
+			} else {
+				remainingItems[remainingItems.length - 1].getElementsByTagName('button')[0].focus();
 			}
+		} else {
+			this.input.focus();
 		}
-	});
 
-	// Event listener for the suggestions box
-	suggestionsBox.addEventListener('click', function (e) {
-		// Add the clicked suggestion to the selected items list
-		if (e.target && e.target.classList.contains('suggestion-btn')) {
-			addSelectedItem(e.target.textContent);
-			suggestionsBox.innerHTML = '';
-			multiSelectInput.value = '';
+		this.selectedItems = this.selectedItems
+			.filter((name) => name !== item.firstChild.textContent.trim());
+	}
+
+	addItem(item) {
+		const newItem = document.createElement('li');
+		newItem.textContent = `${item} `;
+		newItem.classList.add(className('a-tag'));
+		newItem.classList.add(className(`${this.baseClassName}__tag`));
+
+		const removeBtn = document.createElement('button');
+		removeBtn.classList.add(className(`${this.baseClassName}-selected-items__remove-btn`));
+
+		const buttonTextContainer = document.createElement('span');
+		buttonTextContainer.classList.add('u-visuallyhidden');
+		removeBtn.appendChild(buttonTextContainer);
+		buttonTextContainer.textContent = `Ta bort ${item}`; // Accessibility label for screen readers
+
+		// Event listener for removing the selected item
+		removeBtn.addEventListener('click', () => {
+			this.removeItem(newItem, Array.from(this.selectedItemsList.children).indexOf(newItem));
+		});
+
+		newItem.appendChild(removeBtn);
+
+		this.selectedItemsList.appendChild(newItem);
+		this.selectedItems.push(item);
+	}
+
+	removeHighlight() {
+		const items = this.suggestionsBox.getElementsByClassName(`${this.baseClassName}__suggestion-btn`);
+
+		[].forEach.call(items, (item) => {
+			item.classList.remove('autocomplete-active');
+		});
+	}
+
+	highlight(direction) {
+		const items = this.suggestionsBox.getElementsByClassName(`${this.baseClassName}__suggestion-btn`);
+		let focus = this.currentFocus;
+
+		if (direction === 'down') {
+			focus = (focus >= items.length - 1) ? 0 : focus + 1;
+		} else {
+			focus = (focus <= 0) ? items.length - 1 : focus - 1;
 		}
-	});
+
+		this.setFocus(focus);
+		this.removeHighlight();
+
+		items[this.currentFocus].classList.add('autocomplete-active');
+		items[this.currentFocus].scrollIntoView({ block: 'nearest' });
+	}
+
+	selectHighlighted() {
+		const items = this.suggestionsBox.getElementsByClassName(`${this.baseClassName}__suggestion-btn`);
+
+		if (this.currentFocus > -1 && items[this.currentFocus]) {
+			this.addItem(items[this.currentFocus].textContent);
+			this.clearSuggestions();
+			this.input.value = '';
+			this.resetFocus();
+		}
+	}
+
+	onKeyDown = (e) => {
+		if (e.keyCode === 40) {
+			this.highlight('down');
+		} else if (e.keyCode === 38) {
+			this.highlight('up');
+		} else if (e.keyCode === 13) {
+			e.preventDefault();
+
+			this.selectHighlighted();
+		}
+	}
+
+	onClick = (e) => {
+		if (e.target.classList.contains(className(`${this.baseClassName}__suggestion-btn`))) {
+			this.addItem(e.target.textContent);
+			this.clearSuggestions();
+			this.input.value = '';
+		}
+	}
 }
 
+const multiSelectElements = document.querySelectorAll('.js-m-multi-select');
+
 if (multiSelectElements) {
-	[].forEach.call(multiSelectElements, setup);
+	[].forEach.call(multiSelectElements, (el) => new MultiSelect(el));
 }
