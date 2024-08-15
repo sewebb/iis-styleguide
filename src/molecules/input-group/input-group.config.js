6557993d52e1af3371d8e8a648632b01c00b10db
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2024-08-15 11:42:14 +0200

    Input group component

---
+++ src/molecules/input-group/input-group.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+	status: 'wip',
+
+	context: {
+		type: 'url',
+		placeholder: 'Website',
+		before: 'https://',
+		after: null,
+		multipleInputs: false,
+	},
+	variants: [
+		{
+			name: 'With after',
+			context: {
+				type: 'email',
+				placeholder: 'firstname.lastname',
+				before: null,
+				after: '\\@internetstiftelsen.se',
+			},
+		},
+		{
+			name: 'With before and after',
+			context: {
+				type: 'number',
+				placeholder: 'Amount',
+				before: '$',
+				after: '.00',
+			},
+		},
+		{
+			name: 'Multiple inputs',
+			context: {
+				multipleInputs: true,
+			},
+		},
+	],
+}
