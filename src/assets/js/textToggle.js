3f3ddc5d7f760628186dec619ebe9ef837caa60a
Robert Hedman <robert.hedman@internetstiftelsen.se>
2024-08-20 12:40:53 +0200

    more variants to Toggle Button

--- src/assets/js/textToggle.js
+++ src/assets/js/textToggle.js
@@ -6,10 +6,15 @@ function toggleTextOnClick(e) {
 	const target = (el.dataset.toggleTarget) ? document.getElementById(el.dataset.toggleTarget) : el;
 	const i = parseInt(el.dataset.iteration || 0, 10);
 	const options = el.dataset.toggleText.split('|');
-	const nextIteration = (i + 1) === options.length ? 0 : i + 1;
+	const nextIteration = (i + 1) === options.length ?	 0 : i + 1;
+	const ariaPressed = el.getAttribute('aria-pressed');
 
 	el.dataset.iteration = nextIteration;
 	target.innerText = options[nextIteration];
+
+	if (ariaPressed) {
+		el.setAttribute('aria-pressed', ariaPressed === 'true' ? 'false' : 'true');
+	}
 }
 
 /* eslint-disable */
