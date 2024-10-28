cfb654c6f3f7516342969ae1b938f6350718c718
Robert Hedman <robert.hedman@internetstiftelsen.se>
2024-10-28 13:38:52 +0100

    delete Modal Graph component

--- src/components.js
+++ src/components.js
@@ -22,7 +22,6 @@ import './molecules/glider/glider-course';
 import './molecules/glider/glider-hero';
 import './molecules/context-menu/context-menu';
 import './molecules/alert/alert';
-import './molecules/modal/modal-graph';
 import './molecules/continue-video-guide/continue-video-guide';
 import './organisms/video-guide/video-guide';
 import './organisms/timeline/timeline';
