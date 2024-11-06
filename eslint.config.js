a3b4c55dc068bbfb1ccfc19138badbea11a33b0a
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2024-11-06 13:27:58 +0100

    Updated eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,5 @@
-// eslint.config.js
-import babelParser from "@babel/eslint-parser";
-import eslintConfig from "@internetstiftelsen/eslint-config";
-import { FlatCompat } from "@eslint/eslintrc";
-import path from "path";
-import { fileURLToPath } from "url";
+import config from "@internetstiftelsen/eslint-config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-	baseDirectory: __dirname
-});
 export default [
-	eslintConfig, {
-		"languageOptions": {
-			"parser": babelParser
-		},
-		"env": {
-			"browser": true
-		},
-		"globals": {
-			"YT": true
-		},
-		"rules": {
-			"no-param-reassign": [2, { "props": false }],
-		},
-		ignores: ["*.conf.js", "*.config.js", "*vendor*"],
-		...compat.extends("eslint-config-airbnb-base"),
-	}
+	...config,
 ];
