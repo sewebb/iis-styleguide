28a2bda3eeaf730312d9acb8389d6fc659ddd746
Robert Hedman <robert.hedman@internetstiftelsen.se>
2025-03-25 13:11:27 +0100

    started Flow Chart Prototype and fixed Video Overlay Image ratio

---
+++ src/molecules/simple-flow-chart/simple-flow-chart.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	status: 'prototype',
+	default: 'jade',
+
+	context: {
+		background_color: '#ffce2e',
+		icon_fill_color: '#1f2a36',
+		icon_background_hover_color: '#ffce2e',
+	},
+	variants: [
+		{
+			name: 'jade',
+			context: {
+				color: 'jade'
+			}
+		},
+		{
+			name: 'ruby',
+			context: {
+				color: 'jade'
+			}
+		},
+	]
+};
