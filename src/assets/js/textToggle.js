0a68cbbcfc75b4fe1e4e01fcccd430a3773fcd95
Tobias Bleckert <hola@tobiasbleckert.se>
2025-10-02 13:15:13 +0200

    Ensure `data-toggle-text` attribute exists before proceeding

--- src/assets/js/textToggle.js
+++ src/assets/js/textToggle.js
@@ -3,13 +3,19 @@ function toggleTextOnClick(e) {
 	e.preventDefault();
 
 	const el = e.target;
+
+	if (!el.dataset.toggleText) {
+		console.warn('No data-toggle-text attribute found on element', el);
+		return;
+	}
+
 	const target = (el.dataset.toggleTarget) ? document.getElementById(el.dataset.toggleTarget) : el;
 	const i = parseInt(el.dataset.iteration || 0, 10);
 	const options = el.dataset.toggleText.split('|');
 	const nextIteration = (i + 1) === options.length ?	 0 : i + 1;
 	const ariaPressed = el.getAttribute('aria-pressed');
 
-	el.dataset.iteration = nextIteration;
+	el.dataset.iteration = nextIteration.toString();
 	target.innerText = options[nextIteration];
 
 	if (ariaPressed) {
@@ -17,7 +23,7 @@ function toggleTextOnClick(e) {
 	}
 }
 
- 
+
 document.addEventListener('click', (e) => {
 	if (e.target.closest('[data-toggle-text]')) {
 		toggleTextOnClick(e);
