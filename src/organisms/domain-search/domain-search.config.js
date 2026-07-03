ceccdd3646a3cc3d882547ce6f343f7704625c8c
Robert Hedman <robert.hedman@internetstiftelsen.se>
2026-07-03 14:46:40 +0200

    removed old Domain Search variants

--- src/organisms/domain-search/domain-search.config.js
+++ src/organisms/domain-search/domain-search.config.js
@@ -2,50 +2,14 @@ module.exports = {
 	status: 'ready',
 
 	context: {
-		hidden: 'false',
-		id: 'domain-search-page',
-		addidional_classes: 'o-domain-search-page--narrow',
-		label: 'sök en .se- eller .nu-domän',
-		white_background: true,
-		result: false,
-		available: false,
+		action: '/',
+		method: 'get',
+		novalidate: false,
+		input_id: 'domain-search-input',
+		label: 'Sök ledig domän',
+		placeholder: 'Sök ledig domän',
+		button_text: 'Sök domän',
+		button_id: 'o-domain-search-submit',
+		value: '',
 	},
-	variants: [
-		{
-			name: 'result-available',
-			context: {
-				result: false,
-				available: true,
-				wildcard: false,
-				duplicate: false,
-			}
-		},
-		{
-			name: 'result-unavailable',
-			context: {
-				result: true,
-				available: false,
-				wildcard: false,
-				duplicate: false,
-			}
-		},
-		{
-			name: 'result-wildcard',
-			context: {
-				result: true,
-				available: false,
-				wildcard: true,
-				duplicate: false,
-			}
-		},
-		{
-			name: 'result-duplicate',
-			context: {
-				result: true,
-				available: false,
-				wildcard: false,
-				duplicate: true,
-			}
-		}
-	]
 };
--- src/pages/domain-search-page/domain-search-page.config.js
+++ src/pages/domain-search-page/domain-search-page.config.js
@@ -1,53 +1,5 @@
 module.exports = {
 	status: 'wip',
 
-	context: {
-		noResult: true,
-		singleResult: false,
-		availableResult: false,
-		wildcardResult: false,
-		duplicateResult: false,
-	},
-	variants: [
-		{
-			name: 'result-available',
-			context: {
-				noResult: false,
-				availableResult: true,
-				singleResult: false,
-				wildcardResult: false,
-				duplicateResult: false
-			}
-		},
-		{
-			name: 'result-unavailable',
-			context: {
-				noResult: false,
-				availableResult: false,
-				singleResult: true,
-				wildcardResult: false,
-				duplicateResult: false
-			}
-		},
-		{
-			name: 'result-wildcard',
-			context: {
-				noResult: false,
-				availableResult: false,
-				singleResult: false,
-				wildcardResult: true,
-				duplicateResult: false,
-			}
-		},
-		{
-			name: 'result-duplicate',
-			context: {
-				noResult: false,
-				availableResult: false,
-				singleResult: false,
-				wildcardResult: false,
-				duplicateResult: true,
-			}
-		}
-	]
+	context: {},
 };
